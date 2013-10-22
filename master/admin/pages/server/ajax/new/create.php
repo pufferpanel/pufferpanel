b11921c83fb07a4cfa03ccea3b76e190725d7094
Dane Everitt <dane@daneeveritt.com>
2013-10-22 17:24:39 -0400

    Redirect with error code

--- master/admin/pages/server/ajax/new/create.php
+++ master/admin/pages/server/ajax/new/create.php
@@ -10,13 +10,13 @@ if($core->framework->auth->isLoggedIn($_SERVER['REMOTE_ADDR'], $core->framework-
  * Are they all Posted?
  */
 if(!isset($_POST['server_name'], $_POST['node'], $_POST['email'], $_POST['server_ip'], $_POST['server_port'], $_POST['alloc_mem'], $_POST['alloc_disk'], $_POST['sftp_pass'], $_POST['sftp_pass_2'], $_POST['backup_disk'], $_POST['backup_files']))
-	$core->framework->page->redirect('../../add.php?error=missing_args');
+	$core->framework->page->redirect('../../add.php?disp=missing_args');
 
 /*
  * Validate Server Name
  */
 if(!preg_match('/^[\w-]{4,35}$/', $_POST['server_name']))
-	$core->framework->page->redirect('../../add.php?error=server_name');
+	$core->framework->page->redirect('../../add.php?error=server_name&disp=s_fail');
 	
 /*
  * Determine if Node (IP & Port) is Avaliable
@@ -29,7 +29,7 @@ $select->execute(array(
 if($select->rowCount() == 1)
 	$node = $select->fetch();
 else
-	$core->framework->page->redirect('../../add.php?error=node');
+	$core->framework->page->redirect('../../add.php?error=node&disp=n_fail');
 
 	/*
 	 * Validate IP & Port
@@ -38,25 +38,25 @@ else
 	$ports = json_decode($node['ports'], true);
 
 	if(!array_key_exists($_POST['server_ip'], $ips))
-		$core->framework->page->redirect('../../add.php?error=server_ip');
+		$core->framework->page->redirect('../../add.php?error=server_ip&disp=ip_fail');
 		
 	if(!array_key_exists($_POST['server_port'], $ports[$_POST['server_ip']]))
-		$core->framework->page->redirect('../../add.php?error=server_port');
+		$core->framework->page->redirect('../../add.php?error=server_port&disp=port_fail');
 		
 	if($ports[$_POST['server_ip']][$_POST['server_port']] == 0)
-		$core->framework->page->redirect('../../add.php?error=server_port');
+		$core->framework->page->redirect('../../add.php?error=server_port&disp=port_full');
 	
 /*
  * Validate Email
  */	
 if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
-	$core->framework->page->redirect('../../add.php?error=email');
+	$core->framework->page->redirect('../../add.php?error=email&disp=e_fail');
 
 $selectEmail = $mysql->prepare("SELECT `id` FROM `users` WHERE `email` = ?");
 $selectEmail->execute(array($_POST['email']));
 
 	if($selectEmail->rowCount() != 1)
-		$core->framework->page->redirect('../../add.php?error=email');
+		$core->framework->page->redirect('../../add.php?error=email&disp=a_fail');
 	else {
 		$oid = $selectEmail->fetch();
 		$oid = $oid['id'];
@@ -66,13 +66,13 @@ $selectEmail->execute(array($_POST['email']));
  * Validate Disk & Memory
  */	
 if(!is_numeric($_POST['alloc_mem']) || !is_numeric($_POST['alloc_disk']))
-	$core->framework->page->redirect('../../add.php?error=alloc_mem|alloc_disk');
+	$core->framework->page->redirect('../../add.php?error=alloc_mem|alloc_disk&disp=m_fail');
 
 /*
  * Validate SFTP Password
  */
 if($_POST['sftp_pass'] != $_POST['sftp_pass_2'] || strlen($_POST['sftp_pass']) < 8)
-	$core->framework->page->redirect('../../add.php?error=sftp_pass|sftp_pass_2');				
+	$core->framework->page->redirect('../../add.php?error=sftp_pass|sftp_pass_2&disp=p_fail');				
 
 $iv = base64_encode(mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_CAST_256, MCRYPT_MODE_CBC), MCRYPT_RAND));
 $_POST['sftp_pass'] = openssl_encrypt($_POST['sftp_pass'], 'AES-256-CBC', file_get_contents(HASH), false, base64_decode($iv));
@@ -81,7 +81,7 @@ $_POST['sftp_pass'] = openssl_encrypt($_POST['sftp_pass'], 'AES-256-CBC', file_g
  * Validate Backup Disk & Files
  */
 if(!is_numeric($_POST['backup_disk']) || !is_numeric($_POST['backup_space']))
-	$core->framework->page->redirect('../../add.php?error=backup_disk|backup_space');
+	$core->framework->page->redirect('../../add.php?error=backup_disk|backup_space&disp=b_fail');
 
 /*
  * Add Server to Database
