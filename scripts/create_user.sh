fe84696f5900289bc51c5845ef042fc7b939ee67
Dane Everitt <dane@daneeveritt.com>
2013-10-23 00:02:38 -0400

    Create User Script Updated

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -8,16 +8,19 @@
 #	$4 = hard_limit
 
 #Create Home Directory
-mkdir -p /srv/servers/$1/server
+mkdir -p /srv/servers/$1
 
 #Add User
-useradd -m -d /srv/servers/$1 -s /usr/bin/rssh $1
+useradd -d /srv/servers/$1 -s /usr/bin/rssh -G rsshusers $1
 
 #Set Password
 echo -e "$2\n$2" | passwd $1
 
 #Set Folder Permissions
-#chown -R $1 /srv/servers/$1/server
+mkdir /srv/servers/$1/server
+chown root.root /srv/servers/$1
+chmod 755 /srv/servers/$1
+chown $1.rsshusers /srv/servers/$1/server
 
 #Set Disk Limits
 setquota -u $1 $3 $4 0 0 -a
