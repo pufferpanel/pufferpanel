7495f04ddecf76f0588869f72ffebdb89e7ac255
Dane Everitt <dane@daneeveritt.com>
2013-10-27 19:56:07 -0400

    Add new node (including the worlds most annoying section of code)

--- master/admin/pages/node/add.php
+++ master/admin/pages/node/add.php
@@ -53,7 +53,34 @@ if($core->framework->auth->isLoggedIn($_SERVER['REMOTE_ADDR'], $core->framework-
 						if(isset($_GET['disp']) && !empty($_GET['disp'])){
 						
 							switch($_GET['disp']){
-							
+								
+								case 'agree_warn':
+									echo '<div class="error-box">You must agree to the node warning before we can create the node.</div>';
+									break;
+								case 'missing_args':
+									echo '<div class="error-box">Not all arguments were passed by the script.</div>';
+									break;
+								case 'n_fail':
+									echo '<div class="error-box">The node name does not meet the requirements (1-15 characters, a-zA-Z0-9_.-).</div>';
+									break;
+								case 'url_fail':
+									echo '<div class="error-box">The node URL provided is not valid.</div>';
+									break;
+								case 'ip_fail':
+									echo '<div class="error-box">The IP addresses provided were not valid.</div>';
+									break;
+								case 'dir_fail':
+									echo '<div class="error-box">The director(y/ies) you entered were not valid. They must end with a trailing slash.</div>';
+									break;
+								case 'fir_match_fail':
+									echo '<div class="error-box">The main server directory and backup directory cannot be the same, and the backup directory cannot be located inside the main server directory.</div>';
+									break;
+								case 'user_fail':
+									echo '<div class="error-box">SSH users must not be blank, and may not be \'root\'.</div>';
+									break;
+								case 'pass_fail':
+									echo '<div class="error-box">SSH passwords must be at least 12 characters.</div>';
+									break;
 							
 							}
 						
@@ -64,11 +91,12 @@ if($core->framework->auth->isLoggedIn($_SERVER['REMOTE_ADDR'], $core->framework-
 							<form action="ajax/new/create.php" method="POST">
 								<p>
 									<label for="node_name">Node Short Name</label>
-									<input type="text" autocomplete="off" name="node_name" placeholder="nyc-1" class="round default-width-input" />
+									<input type="text" autocomplete="off" name="node_name" placeholder="1.nyc.us" class="round default-width-input" />
+									<em>15 character maximum (a-zA-Z0-9_-.)
 								</p>
 								<p>
 									<label for="node_url">Node URL</label>
-									<input type="text" autocomplete="off" name="node_url" placeholder="http://nyc-1.example.com" class="round default-width-input" />
+									<input type="text" autocomplete="off" name="node_url" placeholder="http://1.nyc.us.example.com" class="round default-width-input" />
 								</p>
 								<div class="stripe-separator"><!--  --></div>
 								<p>
@@ -78,6 +106,7 @@ if($core->framework->auth->isLoggedIn($_SERVER['REMOTE_ADDR'], $core->framework-
 								<p>
 									<label for="node_sftp_ip">Node SFTP IP Address</label>
 									<input type="text" autocomplete="off" name="node_sftp_ip" class="round default-width-input" />
+									<em>In most cases this is the same as the Node IP Address</em>
 								</p>
 								<p>
 									<label for="s_dir">Server Directory</label>
@@ -86,6 +115,7 @@ if($core->framework->auth->isLoggedIn($_SERVER['REMOTE_ADDR'], $core->framework-
 								<p>
 									<label for="s_dir_backup">Server Backup Directory</label>
 									<input type="text" autocomplete="off" name="s_dir_backup" placeholder="/second/backups/" class="round default-width-input" />
+									<em>Remote backup directories are not <strong>currently</strong> supported.</em>
 								</p>
 								<div class="stripe-separator"><!--  --></div>
 								<div class="warning-box round" style="display: none;" id="gen_pass"></div>
@@ -98,6 +128,13 @@ if($core->framework->auth->isLoggedIn($_SERVER['REMOTE_ADDR'], $core->framework-
 									<input type="password" autocomplete="off" name="ssh_pass" class="round default-width-input" />
 								</p>
 								<div class="stripe-separator"><!--  --></div>
+								<p>
+									<label for="ip_port">Available IPs &amp; Ports</label>
+									<textarea name="ip_port" class="round full-width-input" rows="10" placeholder="127.0.0.1|25565,25566,25567,25568,25569,25570"></textarea>
+									<em>Enter one IP address per line, followed by a pipe (|) and then a list of each available port separated with commas.</em>
+								</p>
+								<div class="stripe-separator"><!--  --></div>
+								<div class="warning-box round"><input type="checkbox" name="read_warning" /> By checking this box you are confirming that you have correctly set up your node to handle Minecraft&trade; servers created from this system. Do not add this node until you have correctly done so. Please consult the <a href="#documentation-404">documentation</a> for how to do this if you are unsure.</div>
 								<input type="submit" value="Create Node" class="round blue ic-right-arrow" />
 							</form>
 						</fieldset>
@@ -120,7 +157,6 @@ if($core->framework->auth->isLoggedIn($_SERVER['REMOTE_ADDR'], $core->framework-
 		
 			var field = $.urlParam('error');
 			var exploded = field.split('|');
-			$("#errorDetected").slideDown();
 			
 				$.each(exploded, function(key, value) {
 					
--- master/admin/pages/node/ajax/new/create.php
+++ master/admin/pages/node/ajax/new/create.php
@@ -1,5 +1,111 @@
 <?php
+session_start();
+error_reporting('E_ALL');
+require_once('../../../../../core/framework/framework.core.php');
 
-echo 'lol wut';
+if($core->framework->auth->isLoggedIn($_SERVER['REMOTE_ADDR'], $core->framework->auth->getCookie('pp_auth_token'), true) !== true){
+	exit('<div class="error-box round">Failed to Authenticate Account.</div>');
+}
+	
+//Cookies :3
+setcookie("__TMP_pp_admin_newnode", json_encode($_POST), time() + 10, '/', $core->framework->settings->get('cookie_website'));
+
+/*
+ * Agree Warning
+ */
+if(!isset($_POST['read_warning']))
+	$core->framework->page->redirect('../../add.php?disp=agree_warn');
+
+/*
+ * Are they all Posted?
+ */
+if(!isset($_POST['node_name'], $_POST['node_url'], $_POST['node_ip'], $_POST['node_sftp_ip'], $_POST['s_dir'], $_POST['s_dir_backup'], $_POST['ssh_user'], $_POST['ssh_pass'], $_POST['ip_port']))
+	$core->framework->page->redirect('../../add.php?disp=missing_args');
+
+/*
+ * Validate Node Name
+ */
+if(!preg_match('/^[\w.-]{1,15}$/', $_POST['node_name']))
+	$core->framework->page->redirect('../../add.php?error=node_name&disp=n_fail');
+	
+/*
+ * Validate Node URL
+ */
+if(!preg_match('/^[https:\/\/]+[\w\d._-]+[\:]{0,1}[\d]{0,6}[\/]{1}$/', $_POST['node_url']))
+	$core->framework->page->redirect('../../add.php?error=node_url&disp=url_fail');
+	
+/*
+ * Validate node_ip & node_sftp_ip
+ */
+if(!filter_var($_POST['node_ip'] , FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) || !filter_var($_POST['node_sftp_ip'] , FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE))
+	$core->framework->page->redirect('../../add.php?error=node_ip|node_sftp_ip&disp=ip_fail');
+
+if(!preg_match('/^[\w.\/-]+[^\/][\/]{1}$/', $_POST['s_dir']) || !preg_match('/^[\w.\/-]+[^\/][\/]{1}$/', $_POST['s_dir_backup']))
+	$core->framework->page->redirect('../../add.php?error=s_dir|s_dir_backup&disp=dir_fail');
+	
+if($_POST['s_dir'] == $_POST['s_dir_backup'])
+	$core->framework->page->redirect('../../add.php?error=s_dir|s_dir_backup&disp=dir_match_fail');
+	
+if(strlen($_POST['ssh_user']) < 1 || $_POST['ssh_user'] == 'root')
+	$core->framework->page->redirect('../../add.php?error=ssh_user&disp=user_fail');
+
+if(strlen($_POST['ssh_pass']) < 12)
+	$core->framework->page->redirect('../../add.php?error=ssh_pass&disp=pass_fail');
+
+/*
+ * Process IPs and Ports
+ */
+$IPP = array();
+$IPA = array();
+$lines = explode("\r\n", $_POST['ip_port']);
+foreach($lines as $id => $values)
+	{
+	
+		list($ip, $ports) = explode('|', $values);
+		
+		$IPA = array_merge($IPA, array($ip => array()));
+		$IPP = array_merge($IPP, array($ip => array()));
+		
+		$ports = explode(',', $ports);
+
+		for($l=0; $l<count($ports); $l++)
+			{
+							
+				$IPP[$ip] = array_merge($IPP[$ip], array($ports[$l] => $ports[$l]));
+			
+			}
+		
+		/*
+		 * Ghetto Function to Flip the Array since PHP wants to set the keys equal to random numbers and not the port.
+		 */
+		$IPP[$ip] = array_flip($IPP[$ip]);
+		$IPP[$ip] = array_fill_keys(array_keys($IPP[$ip]), 1);
+		
+		$IPA[$ip] = array_merge($IPA[$ip], array("ports_free" => count($IPP[$ip]), "ports_used" => 0, "ports_suspended" => 0));
+			
+	}
+	
+$IPA = json_encode($IPA);
+$IPP = json_encode($IPP);
+
+$iv = base64_encode(mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_CAST_256, MCRYPT_MODE_CBC), MCRYPT_RAND));
+$_POST['ssh_pass'] = openssl_encrypt($_POST['ssh_pass'], 'AES-256-CBC', file_get_contents(HASH), false, base64_decode($iv));
+
+$create = $mysql->prepare("INSERT INTO `nodes` VALUES(NULL, :name, :link, :ip, :sftp_ip, :sdir, :bdir, :suser, :iv, :spass, :ips, :ports)");
+$create->execute(array(
+	':name' => $_POST['node_name'],
+	':link' => $_POST['node_url'],
+	':ip' => $_POST['node_ip'],
+	':sftp_ip' => $_POST['node_sftp_ip'],
+	':sdir' => $_POST['s_dir'],
+	'bdir' => $_POST['s_dir_backup'],
+	':suser' => $_POST['ssh_user'],
+	':iv' => $iv,
+	':spass' => $_POST['ssh_pass'],
+	':ips' => $IPA,
+	':ports' => $IPP
+));
+
+$core->framework->page->redirect('../../view.php?id='.$mysql->lastInsertId());
 
 ?>
