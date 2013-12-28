06a4b2457167551013d9babd6afaa5de3200aa01
Dane Everitt <dane@daneeveritt.com>
2013-12-28 14:39:26 -0500

    Update start script for 1.7+ only

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -22,7 +22,7 @@
 #	$2 = max_ram
 #	$3 = run_as_user
 
-invoke="java -Xmx$2M -Xincgc -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -jar server.jar --nojline"
+invoke="java -Xmx$2M -Xincgc -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -jar server.jar -o true"
 su - $3 -s /bin/bash -c "cd $1; screen -S minecraft_$3 -t bukkit_$3 -dm $invoke; exit;"
 
 exit 0
