a405f8436cb480e0a4e989de8b646905675632c8
mantux5 <mantassabaliauskas22@gmail.com>
2014-01-01 22:31:26 +0200

    Changing how default server works |Server creation

--- master/admin/pages/server/ajax/new/create.php
+++ master/admin/pages/server/ajax/new/create.php
@@ -149,7 +149,7 @@ $mysql->prepare("UPDATE `nodes` SET `ports` = :ports")->execute(array(':ports' =
 $con = ssh2_connect($node['sftp_ip'], 22);
 ssh2_auth_password($con, $node['username'], openssl_decrypt($node['password'], 'AES-256-CBC', file_get_contents(HASH), 0, base64_decode($node['encryption_iv'])));
 
-$stream = ssh2_exec($con, 'cd /srv/scripts; sudo ./create_user.sh '.$ftpUser.' '.$_POST['sftp_pass_2'].' '.($_POST['alloc_disk'] - 1024).' '.$_POST['alloc_disk'].' '$node['default_srv'], true);
+$stream = ssh2_exec($con, 'cd /srv/scripts; sudo ./create_user.sh '.$ftpUser.' '.$_POST['sftp_pass_2'].' '.($_POST['alloc_disk'] - 1024).' '.$_POST['alloc_disk'], true);
 
 $core->framework->email->buildEmail('admin_new_server', array(
         'NAME' => $_POST['server_name'],
