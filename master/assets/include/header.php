8757192dae383dea12314de45a1aa973ce861506
mantux5 <mantassabaliauskas22@gmail.com>
2014-01-03 01:35:53 +0200

    A little fix :P

--- master/assets/include/header.php
+++ master/assets/include/header.php
@@ -1,3 +1,4 @@
+<meta charset="UTF-8">
 <link rel="stylesheet" href="<?php echo $core->framework->settings->get('master_url'); ?>assets/css/bootstrap.css">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src="<?php echo $core->framework->settings->get('master_url'); ?>assets/javascript/bootstrap.min.js"></script>
