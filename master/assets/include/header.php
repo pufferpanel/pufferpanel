dd1bdb482f0b4fc4d48cb5a99dfc7c779bd72ce5
Dane Everitt <dane@daneeveritt.com>
2014-01-02 23:16:44 -0500

    Updated files for Admin CP. Added function to automatically add active class based on page URL. Manual fallback for some pages.

--- master/assets/include/header.php
+++ master/assets/include/header.php
@@ -1,3 +1,4 @@
-<link rel="stylesheet" href="assets/css/bootstrap.css">
+<link rel="stylesheet" href="<?php echo $core->framework->settings->get('assets_url'); ?>css/bootstrap.css">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="assets/javascript/bootstrap.min.js"></script>
+<script src="<?php echo $core->framework->settings->get('assets_url'); ?>javascript/bootstrap.min.js"></script>
+<script src="<?php echo $core->framework->settings->get('assets_url'); ?>javascript/admin.min.js"></script>
