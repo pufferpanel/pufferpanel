b6f2bb53036ed8aa9a71afe6cd6a63817709fde5
Dane Everitt <dane@daneeveritt.com>
2014-01-09 14:18:36 -0500

    Add logout and servers buttons to header in ACP

--- master/admin/index.php
+++ master/admin/index.php
@@ -35,6 +35,17 @@ if($core->framework->auth->isLoggedIn($_SERVER['REMOTE_ADDR'], $core->framework-
 			<div class="navbar-header">
 				<a class="navbar-brand" href="#"><?php echo $core->framework->settings->get('company_name'); ?></a>
 			</div>
+			<div class="navbar-collapse navbar-responsive-collapse collapse" style="height: 1px;">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Account <b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a href="<?php echo $core->framework->settings->get('master_url'); ?>logout.php">Logout</a></li>
+								<li><a href="<?php echo $core->framework->settings->get('master_url'); ?>servers.php">View All Servers</a></li>
+							</ul>
+					</li>
+				</ul>
+			</div>
 		</div>
 		<div class="row">
 			<div class="col-3"><?php include('../assets/include/admin.php'); ?></div>
