c900a454b45c741465bce71c895ccf569590fc9b
Dane Everitt <dane@daneeveritt.com>
2014-01-17 20:47:59 -0500

    Push to v0.5.0.1-beta

--- node/assets/include/footer.php
+++ node/assets/include/footer.php
@@ -1,2 +1,2 @@
-<div class="col-8 nopad"><p>PufferPanel is licensed under a <a href="https://github.com/DaneEveritt/PufferPanel/blob/master/LICENSE">GPL-v3 License</a>.<br />Running Version 0.5 Beta distributed by <a href="http://kelp.in">Kelpin' Systems</a>.</p></div>
+<div class="col-8 nopad"><p>PufferPanel is licensed under a <a href="https://github.com/DaneEveritt/PufferPanel/blob/master/LICENSE">GPL-v3 License</a>.<br />Running Version 0.5.0.1 Beta distributed by <a href="http://kelp.in">Kelpin' Systems</a>.</p></div>
 <div class="col-4 nopad-right"><p class="pull-right">Page Generated in <?php echo number_format((microtime(true) - $pageStartTime), 4); ?> seconds.<br />Executed <?php echo $core->framework->page->override_getCount(); ?> database queries.</p></div>
