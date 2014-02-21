8b491f51ae2bf174eba2786a8a5be5288661aa7e
Dane Everitt <DaneEveritt@Danes-MacBook-Pro.local>
2014-02-21 15:14:06 -0500

    Add missing parenthesis

--- master/admin/pages/node/ajax/new/create.php
+++ master/admin/pages/node/ajax/new/create.php
@@ -98,7 +98,7 @@ foreach($lines as $id => $values)
 				/*
 				 * Validate Port Spacing
 				 */
-				if(!array_key_exists($l - 1, $IPP[$ip]) && !array_key_exists($l + 1, $IPP[$ip])
+				if(!array_key_exists($l - 1, $IPP[$ip]) && !array_key_exists($l + 1, $IPP[$ip]))
 					$IPP[$ip][$ports[$l]] = 1;
 			
 			}
