04d7a34f7a13c4c777d48f9ed834d43a0c3ef0cd
Chomiciak <chomiciak@gmail.com>
2014-04-10 22:22:36 +0200

    Update create.php
    There was missing ;

--- panel/admin/pages/server/ajax/new/create.php
+++ panel/admin/pages/server/ajax/new/create.php
@@ -198,7 +198,7 @@ $mysql->prepare("UPDATE `nodes` SET `ports` = :ports")->execute(array(':ports' =
 		)
 	);
 	
-	$context = context_create_stream($context_options)
+	$context = context_create_stream($context_options);
 	$fp = fopen('http://'.$node['sftp_ip'].':8003/gameservers/', 'r', false, $context);
 	$content = stream_get_contents($fp);
 	fclose($fp);
