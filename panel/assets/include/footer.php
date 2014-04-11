a8b9db482fb8edcd3b18dc604abd3c5a3cacc4b8
Dane Everitt <DaneEveritt@Danes-MacBook-Pro.local>
2014-04-10 22:11:21 -0400

    Push to 0.6.0 Alpha (RC2)

--- panel/assets/include/footer.php
+++ panel/assets/include/footer.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="col-8 ">
-		<p>PufferPanel is licensed under a <a href="https://github.com/DaneEveritt/PufferPanel/blob/master/LICENSE">GPL-v3 License</a>.<br />Running Version 0.6.0 Alpha (R1) distributed by <a href="http://kelp.in">Kelpin' Systems</a>.</p>
+		<p>PufferPanel is licensed under a <a href="https://github.com/DaneEveritt/PufferPanel/blob/master/LICENSE">GPL-v3 License</a>.<br />Running Version 0.6.0 Alpha (RC2) distributed by <a href="http://kelp.in">Kelpin' Systems</a>.</p>
 	</div>
 	<div class="col-4">
 		<p class="pull-right">Page Generated in <?php echo number_format((microtime(true) - $pageStartTime), 4); ?> seconds.<br />Executed <?php echo $core->page->override_getCount(); ?> database queries.</p>
