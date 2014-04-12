3b7fb67bce09c6a0e03146457f1786bcd86697c5
Dane Everitt <DaneEveritt@Danes-MacBook-Pro.local>
2014-04-11 21:27:03 -0400

    Push to RC3

--- panel/admin/install/do/tables.php
+++ panel/admin/install/do/tables.php
@@ -217,7 +217,7 @@ if(file_exists('../install.lock'))
 			</div>
 		</div>
 		<div class="footer">
-			<div class="col-8 nopad"><p>PufferPanel is licensed under a <a href="https://github.com/DaneEveritt/PufferPanel/blob/master/LICENSE">GPL-v3 License</a>.<br />Running Version 0.6.0 Alpha (RC2) distributed by <a href="http://kelp.in">Kelpin' Systems</a>.</p></div>
+			<div class="col-8 nopad"><p>PufferPanel is licensed under a <a href="https://github.com/DaneEveritt/PufferPanel/blob/master/LICENSE">GPL-v3 License</a>.<br />Running Version 0.6.0 Alpha (RC3) distributed by <a href="http://kelp.in">Kelpin' Systems</a>.</p></div>
 		</div>
 	</div>
 </body>
