a4a6ba2ac5f0988cbc191ac185d36b9e140adfae
Chomiciak <chomiciak@gmail.com>
2014-04-15 19:03:11 +0200

    Update passgen.php

--- panel/node/ajax/settings/passgen.php
+++ panel/node/ajax/settings/passgen.php
@@ -2,7 +2,7 @@
 session_start();
 require_once('../../../core/framework/framework.core.php');
 
-if($core->auth->isLoggedIn($_SERVER['REMOTE_ADDR'], $core->auth->getCookie('pp_auth_token'), null, true) !== true){
+if($core->auth->isLoggedIn($_SERVER['REMOTE_ADDR'], $core->auth->getCookie('pp_auth_token'), $core->auth->getCookie('pp_server_hash')) === true){
 	$core->page->redirect('../../../index.php');
 }
 
