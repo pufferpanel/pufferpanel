f33063ea4977dfeb533a8c9e6fff0703e31427d9
Dane Everitt <DaneEveritt@Danes-MacBook-Pro.local>
2014-04-19 17:52:26 -0400

    Node index is now multi-language

--- panel/node/index.php
+++ panel/node/index.php
@@ -37,73 +37,67 @@ if($core->auth->isLoggedIn($_SERVER['REMOTE_ADDR'], $core->auth->getCookie('pp_a
 		<div class="row">
 			<div class="col-3">
 				<div class="list-group">
-					<a href="#" class="list-group-item list-group-item-heading"><strong>Account Actions</strong></a>
-					<a href="../account.php" class="list-group-item">Settings</a>
-					<a href="../servers.php" class="list-group-item">My Servers</a>
+					<a href="#" class="list-group-item list-group-item-heading"><strong><?php echo $_l->tpl('sidebar.acc_actions'); ?></strong></a>
+					<a href="../account.php" class="list-group-item"><?php echo $_l->tpl('sidebar.settings'); ?></a>
+					<a href="../servers.php" class="list-group-item"><?php echo $_l->tpl('sidebar.servers'); ?></a>
 				</div>
 				<div class="list-group">
-					<a href="#" class="list-group-item list-group-item-heading"><strong>Server Actions</strong></a>
-					<a href="index.php" class="list-group-item active">Overview</a>
-					<a href="console.php" class="list-group-item">Live Console</a>
-					<a href="files/index.php" class="list-group-item">File Manager</a>
+					<a href="#" class="list-group-item list-group-item-heading"><strong><?php echo $_l->tpl('sidebar.server_acc'); ?></strong></a>
+					<a href="index.php" class="list-group-item active"><?php echo $_l->tpl('sidebar.overview'); ?></a>
+					<a href="console.php" class="list-group-item"><?php echo $_l->tpl('sidebar.console'); ?></a>
+					<a href="files/index.php" class="list-group-item"><?php echo $_l->tpl('sidebar.files'); ?></a>
 				</div>
 				<div class="list-group">
-					<a href="#" class="list-group-item list-group-item-heading"><strong>Server Settings</strong></a>
+					<a href="#" class="list-group-item list-group-item-heading"><strong><?php echo $_l->tpl('sidebar.server_sett'); ?></strong></a>
 					
-					<a href="settings.php" class="list-group-item">Server Management</a>
-					<a href="plugins/index.php" class="list-group-item">Server Plugins</a>
+					<a href="settings.php" class="list-group-item"><?php echo $_l->tpl('sidebar.manage'); ?></a>
+					<a href="plugins/index.php" class="list-group-item"><?php echo $_l->tpl('sidebar.plugins'); ?></a>
 				</div>
 			</div>
 			<div class="col-9">
 				<div class="col-12">
-					<h3 class="nopad">Stats Overview</h3><hr />
-					<div id="online_notice" class="alert alert-info"><i class="fa fa-spinner fa-spin"></i> Attempting to collect usage information, this could take a few seconds. Please ensure server is online.</div>
+					<h3 class="nopad"><?php echo $_l->tpl('node.overview.stats_h1'); ?></h3><hr />
+					<div id="online_notice" class="alert alert-info"><i class="fa fa-spinner fa-spin"></i> <?php echo $_l->tpl('node.overview.collect_usage'); ?></div>
 					<span id="toggle_on" style="display:none;">
-						<h5>CPU Usage</h5>
+						<h5><?php echo $_l->tpl('node.overview.cpu_h5'); ?></h5>
 							<div class="progress">
-								<div class="progress-bar" id="cpu_bar" style="width:100%;max-width:100%;">Gathering...</div>
+								<div class="progress-bar" id="cpu_bar" style="width:100%;max-width:100%;"></div>
 							</div>
-						<h5>Memory Usage</h5>
+						<h5><?php echo $_l->tpl('node.overview.memory_h5'); ?></h5>
 							<div class="progress">
-								<div class="progress-bar" id="memory_bar" style="width:100%; max-width:100%;">Gathering...</div>
+								<div class="progress-bar" id="memory_bar" style="width:100%; max-width:100%;"></div>
 							</div>
 					</span>
-					<h5>Players Online</h5>
-						<div id="players_notice" class="alert alert-info"><i class="fa fa-spinner fa-spin"></i> Attempting to collect player information, this could take a few seconds. Please ensure server is online.</div>
+					<h5><?php echo $_l->tpl('node.overview.players_h5'); ?></h5>
+						<div id="players_notice" class="alert alert-info"><i class="fa fa-spinner fa-spin"></i> <?php echo $_l->tpl('node.overview.collect_players'); ?></div>
 						<span id="toggle_players" style="display:none;">
-							<p class="text-muted">No players are currently online.</p>
+							<p class="text-muted"><?php echo $_l->tpl('node.overview.no_players'); ?></p>
 						</span>
 				</div>
 				<div class="col-12">
-					<h3>Disk Space Used</h3><hr />
+					<h3><?php echo $_l->tpl('node.overview.disk_h1'); ?></h3><hr />
 					<div id="server_stats">
-						<div class="alert alert-info"><i class="fa fa-spinner fa-spin"></i> Collecting disk usage information, this could take a few seconds.</div>
+						<div class="alert alert-info"><i class="fa fa-spinner fa-spin"></i> <?php echo $_l->tpl('node.overview.collect_disk'); ?></div>
 					</div>
 				</div>
 				<div class="col-12">
-					<h3>Server Information</h3><hr />
+					<h3><?php echo $_l->tpl('node.overview.information_h1'); ?></h3><hr />
 					<table class="table table-striped table-bordered table-hover">
-						<thead>
-							<tr>
-								<th>Information</th>
-								<th>Data</th>
-							</tr>
-						</thead>
 						<tbody>
 							<tr>
-								<td><strong>Connection</strong></td>
+								<td><strong><?php echo $_l->tpl('string.connection'); ?></strong></td>
 								<td><?php echo $core->server->getData('server_ip').':'.$core->server->getData('server_port'); ?></td>
 							</tr>
 							<tr>
-								<td><strong>Node</strong></td>
+								<td><strong><?php echo $_l->tpl('string.node'); ?></strong></td>
 								<td><?php echo $core->settings->nodeName($core->server->getData('node')); ?></td>
 							</tr>
 							<tr>
-								<td><strong>Memory Allocated</strong></td>
+								<td><strong><?php echo $_l->tpl('string.mem_alloc'); ?></strong></td>
 								<td><?php echo $core->server->getData('max_ram').' MB'; ?></td>
 							</tr>
 							<tr>
-								<td><strong>Disk Allocated</strong></td>
+								<td><strong><?php echo $_l->tpl('string.disk_alloc'); ?></strong></td>
 								<td><?php echo $core->server->getData('disk_space').' MB'; ?></td>
 							</tr>
 						</tbody>
