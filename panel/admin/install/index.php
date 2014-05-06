07dbcbd88eefdf9a70ed6d094c5c2a8c1e878b67
Dane Everitt <DaneEveritt@users.noreply.github.com>
2014-05-06 12:01:29 -0400

    Remove debugging from installer

--- panel/admin/install/index.php
+++ panel/admin/install/index.php
@@ -16,14 +16,6 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see http://www.gnu.org/licenses/.
  */
-/*
- * Debug
- * To debug on a non-local environment (do ot do this publicly!) change Debugger::DETECT to Debugger::PRODUCTION
- */
-use Tracy\Debugger;
-Debugger::enable(Debugger::DETECT, dirname(__DIR__).'/logs');
-Debugger::$strictMode = TRUE;
-
 if(file_exists('install.lock'))
 	exit('Installer is Locked.');
 ?>
