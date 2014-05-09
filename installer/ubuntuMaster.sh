683b34dc0a63f1a865dd58f8929d0b87fe29b929
Chomiciak <chomiciak@gmail.com>
2014-05-09 21:42:11 +0200

    Create ubuntuMaster.sh
    This is it :D

---
+++ installer/ubuntuMaster.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+#############################################################
+#                                                           #
+#                       PufferPanel                         #
+#                                                           #
+#               Ubuntu installer by Chomiciak               #
+#                                                           #
+# Use on the same license as Panel's (and on your own risk!)#
+#                                                           #
+#############################################################
+#                                                           #
+#                 This is Master installer                  #
+#                                                           #
+#############################################################
+
+# Info...
+echo "You must configure some software... I will help you a little. Wait 7 seconds..."
+# Wait...
+read -t 7
+
+# Update!
+apt-get update
+
+# Apache2 install
+apt-get install apache2
+
+# Php5 install
+apt-get install php5
+
+# Mysql install
+apt-get install mysql-server
+
+# Apache-mysql
+apt-get install libapache2-mod-auth-mysql
+
+# Php-mysql
+apt-get install php5-mysql
+
+# SSH2-php
+apt-get install libssh2-1-dev libssh2-php
+
+# pdo-php
+pecl install pdo
+
+# mcrypt-php
+pecl install
+
+# Apache restart
+/etc/init.d/apache2 restart
