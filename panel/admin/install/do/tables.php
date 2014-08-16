6770cf1f78e64be7ffbd973da2674c3dbbb13c6b
Dane Everitt <DaneEveritt@users.noreply.github.com>
2014-08-16 14:16:26 -0400

    throw all errors to page

--- panel/admin/install/do/tables.php
+++ panel/admin/install/do/tables.php
@@ -16,6 +16,9 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see http://www.gnu.org/licenses/.
  */
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 if(file_exists('../install.lock'))
 	exit('Installer is Locked.');
 ?>
