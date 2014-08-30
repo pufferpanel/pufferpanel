5000c7dd1c4905a5f97d90ff536bec6be983719b
Dane Everitt <DaneEveritt@users.noreply.github.com>
2014-08-30 00:12:11 -0400

    Fixed footer

--- src/include/footer.php
+++ src/include/footer.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="col-8 ">
-		<p><?php echo $_l->tpl('footer.license'); ?><br /><?php echo sprintf($_l->tpl('footer.version'), '0.6.1 Beta'); ?></p>
+		<p><?php echo $_l->tpl('footer.license'); ?><br /><?php echo sprintf($_l->tpl('footer.version'), '0.7.2 Beta'); ?></p>
 	</div>
 	<div class="col-4">
 		<p class="pull-right"><?php echo sprintf($_l->tpl('footer.generated'), number_format((microtime(true) - $pageStartTime), 4)); ?><br /><?php echo sprintf($_l->tpl('footer.queries'), Database\databaseInit::getCount()); ?></p>
