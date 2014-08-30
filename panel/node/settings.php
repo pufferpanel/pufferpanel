f8fca5327b62c54ebcd4322b7f409f21ce805eb1
Dane Everitt <DaneEveritt@users.noreply.github.com>
2014-08-29 23:18:38 -0400

    Removed mod pack stuff

--- panel/node/settings.php
+++ panel/node/settings.php
@@ -2,17 +2,17 @@
 /*
     PufferPanel - A Minecraft Server Management Panel
     Copyright (c) 2013 Dane Everitt
- 
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
- 
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
- 
+
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see http://www.gnu.org/licenses/.
  */
@@ -23,34 +23,11 @@ if($core->auth->isLoggedIn($_SERVER['REMOTE_ADDR'], $core->auth->getCookie('pp_a
 
 	Page\components::redirect($core->settings->get('master_url').'index.php?login');
 	exit();
-	
+
 }
 
 if(isset($_GET['do']) && $_GET['do'] == 'generate_password')
-	exit($core->auth->keygen(rand(12, 18)));
-
-
-$packs = $mysql->prepare("SELECT hash, name, version, deleted FROM `modpacks` WHERE `hash` = :hash");
-$packs->execute(array(
-	':hash' => $core->server->getData('modpack')
-));
-
-$pack = $packs->fetch();
-$isDeleted = ($pack['deleted'] == 1) ? '[DELETED] ' : null;
-$currentPack = $isDeleted.$pack['name'].' ('.$pack['version'].')';
-
-
-$packs = $mysql->prepare("SELECT hash, name, version, server_jar FROM `modpacks` WHERE `deleted` = 0 AND `min_ram` <= :ram");
-$packs->execute(array(
-	':ram' => $core->server->getData('max_ram')
-));
-
-$allpacks = array();
-while($row = $packs->fetch()){
-	
-	$allpacks = array_merge($allpacks, array($row));
-	
-}
+	exit($core->auth->keygen(mt_rand(12, 18)));
 
 /*
  * Display Page
@@ -60,15 +37,10 @@ echo $twig->render(
 			'server' => array(
 				'gsd_id' => $core->server->getData('gsd_id'),
 				'server_jar' => (str_replace(".jar", "", $core->server->getData('server_jar'))),
-				'ftp_user' => $core->server->getData('ftp_user'),
-				'modpack' => $core->server->getData('modpack')
+				'ftp_user' => $core->server->getData('ftp_user')
 			),
 			'node' => array(
 				'sftp_ip' => $core->server->nodeData('sftp_ip')
-			),
-			'modpack' => array(
-				'current' => $currentPack,
-				'packs' => $allpacks
 			),
 			'footer' => array(
 				'queries' => Database\databaseInit::getCount(),
