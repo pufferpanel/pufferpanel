3b8c4fd07f1fb174ddfe448c860a5e7bd25c5e8d
Dane Everitt <DaneEveritt@users.noreply.github.com>
2014-09-03 00:29:45 -0400

    Twig improvements to ACP (accounts area)

--- panel/admin/account/new.php
+++ panel/admin/account/new.php
@@ -25,118 +25,12 @@ if($core->auth->isLoggedIn($_SERVER['REMOTE_ADDR'], $core->auth->getCookie('pp_a
 
 if(isset($_GET['do']) && $_GET['do'] == 'generate_password')
 	exit($core->auth->keygen(rand(12, 18)));
+
+echo $twig->render(
+        'admin/account/new.html', array(
+            'footer' => array(
+                'queries' => Database\databaseInit::getCount(),
+                'seconds' => number_format((microtime(true) - $pageStartTime), 4)
+            )
+        ));
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<?php include('../../../src/include/header.php'); ?>
-	<title>PufferPanel Admin Control Panel</title>
-</head>
-<body>
-	<div class="container">
-		<div class="navbar navbar-default">
-			<div class="navbar-header">
-				<a class="navbar-brand" href="#"><?php echo $core->settings->get('company_name'); ?></a>
-			</div>
-			<div class="navbar-collapse navbar-responsive-collapse collapse" style="height: 1px;">
-				<ul class="nav navbar-nav navbar-right">
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Account <b class="caret"></b></a>
-							<ul class="dropdown-menu">
-								<li><a href="<?php echo $core->settings->get('master_url'); ?>logout.php">Logout</a></li>
-								<li><a href="<?php echo $core->settings->get('master_url'); ?>servers.php">View All Servers</a></li>
-							</ul>
-					</li>
-				</ul>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-3"><?php include('../../../src/include/admin.php'); ?></div>
-			<div class="col-9">
-				<h3 class="nopad">Create New Account</h3><hr />
-				<?php
-
-					if(isset($_GET['disp']) && !empty($_GET['disp'])){
-
-						switch($_GET['disp']){
-
-							case 'u_fail':
-								echo '<div class="alert alert-danger">The username you entered does not meet the requirements. Must be at least 4 characters, and no more than 35. Username can only contain a-zA-Z0-9_-</div>';
-								break;
-							case 'e_fail':
-								echo '<div class="alert alert-danger">The email you entered is invalid.</div>';
-								break;
-							case 'p_fail':
-								echo '<div class="alert alert-danger">The passwords you entered did not match or were not at least 8 characters.</div>';
-								break;
-							case 'a_fail':
-								echo '<div class="alert alert-danger">Account with that username or email already exists in the system.</div>';
-								break;
-
-						}
-
-					}
-
-				?>
-				<form action="ajax/new/create.php" method="post">
-					<fieldset>
-						<div class="form-group">
-							<label for="username" class="control-label">Username</label>
-							<div>
-								<input type="text" autocomplete="off" name="username" class="form-control" />
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="email" class="control-label">Email</label>
-							<div>
-								<input type="text" autocomplete="off" name="email" class="form-control" />
-							</div>
-						</div>
-						<div id="gen_pass" class="alert alert-success" style="display:none;margin-bottom: 10px;"></div>
-						<div class="form-group col-6 nopad">
-							<label for="pass" class="control-label">Password</label>
-							<div>
-								<input type="password" name="pass" class="form-control" />
-							</div>
-						</div>
-						<div class="form-group col-6 nopad-right">
-							<label for="pass_2" class="control-label">Password Again</label>
-							<div>
-								<input type="password" name="pass_2" class="form-control" />
-							</div>
-						</div>
-						<div class="form-group">
-							<div>
-								<button class="btn btn-primary" type="submit">Create Account</button>
-								<button class="btn btn-default" id="gen_pass_bttn" type="button">Generate Password</button>
-							</div>
-						</div>
-					</fieldset>
-				</form>
-			</div>
-		</div>
-		<div class="footer">
-			<?php include('../../../src/include/footer.php'); ?>
-		</div>
-	</div>
-	<script type="text/javascript">
-		$(document).ready(function(){
-			$("#gen_pass_bttn").click(function(e){
-				e.preventDefault();
-				$.ajax({
-					type: "GET",
-					url: "new.php?do=generate_password",
-					success: function(data) {
-						$("#gen_pass").html('<strong>Generated Password:</strong> '+data);
-						$("#gen_pass").slideDown();
-						$('input[name="pass"]').val(data);
-						$('input[name="pass_2"]').val(data);
-						return false;
-					}
-				});
-				return false;
-			});
-		});
-	</script>
-</body>
-</html>
