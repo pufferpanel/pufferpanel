5fccffe1609c118c71345a9b994e0bbc8f3eff9b
Dane Everitt <DaneEveritt@users.noreply.github.com>
2014-09-05 23:15:57 -0400

    Updated to twig

---
+++ panel/admin/account/view.php
@@ -0,0 +1,85 @@
+<?php
+/*
+    PufferPanel - A Minecraft Server Management Panel
+    Copyright (c) 2013 Dane Everitt
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+session_start();
+require_once('../../../src/framework/framework.core.php');
+
+if($core->auth->isLoggedIn($_SERVER['REMOTE_ADDR'], $core->auth->getCookie('pp_auth_token'), null, true) !== true){
+	Page\components::redirect('../../index.php?login');
+}
+
+if(isset($_GET['do']) && $_GET['do'] == 'generate_password')
+	exit($core->auth->keygen(rand(12,18)));
+
+if(!isset($_GET['id']))
+	Page\components::redirect('find.php');
+
+/*
+ * Select User Information
+ */
+$select = $mysql->prepare("SELECT * FROM `users` WHERE `id` = :id LIMIT 1");
+$select->execute(array(
+	':id' => $_GET['id']
+));
+
+	if($select->rowCount() != 1)
+		Page\components::redirect('find.php?error=no_user');
+	else
+		$user = $select->fetch();
+
+$date1 = new DateTime(date('Y-m-d', $user['register_time']));
+$date2 = new DateTime(date('Y-m-d', time()));
+
+$user['register_time'] = date('F j, Y g:ia', $user['register_time']).' ('.$date2->diff($date1)->format("%a Days Ago").')';
+
+/*
+ * Select Servers Owned by the User
+ */
+$select = $mysql->prepare("SELECT * FROM `servers` WHERE `owner_id` = :id ORDER BY `active` DESC");
+$select->execute(array(
+	':id' => $user['id']
+));
+
+/*
+ * Iterate through Servers
+ */
+$servers = array();
+while($row = $select->fetch()){
+
+	$servers = array_merge($servers, array(array(
+		"id" => $row['id'],
+		"hash" => $row['hash'],
+		"node" => $core->settings->nodeName($row['node']),
+		"ip" => $row['server_ip'],
+		"port" => $row['server_port'],
+		"name" => $row['name'],
+		"active" => ($row['active'] == '1') ? true : false,
+	)));
+
+}
+
+echo $twig->render(
+		'admin/account/view.html', array(
+			'user' => $user,
+			'servers' => $servers,
+			'footer' => array(
+				'queries' => Database\databaseInit::getCount(),
+				'seconds' => number_format((microtime(true) - $pageStartTime), 4)
+			)
+		));
+?>
