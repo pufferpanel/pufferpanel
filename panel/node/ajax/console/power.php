1c2f0738e750df2c522cfbb26ccd9bf838079dd0
Dane Everitt <DaneEveritt@users.noreply.github.com>
2014-10-18 00:48:49 -0400

    Finalize console power permissions

--- panel/node/ajax/console/power.php
+++ panel/node/ajax/console/power.php
@@ -21,6 +21,9 @@ require_once('../../../../src/framework/framework.core.php');
 
 if($core->auth->isLoggedIn($_SERVER['REMOTE_ADDR'], $core->auth->getCookie('pp_auth_token'), $core->auth->getCookie('pp_server_hash')) === true){
 
+	if($core->user->hasPermission('console.power') !== true)
+		exit('You do not have the required permissions to perform this function.');
+
 	/*
 	 * Open Stream for Reading/Writing
 	 */
