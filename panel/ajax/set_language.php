e1810ae6e2ae77cf12af1a0e1b82e1568042fa06
Dane Everitt <DaneEveritt@users.noreply.github.com>
2014-10-29 14:57:18 -0400

    Update language setter

--- panel/ajax/set_language.php
+++ panel/ajax/set_language.php
@@ -2,17 +2,17 @@
 /*
     PufferPanel - A Minecraft Server Management Panel
     Copyright (c) 2013 Dane Everitt
- 
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
- 
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
- 
+
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see http://www.gnu.org/licenses/.
  */
@@ -26,26 +26,26 @@ if($core->auth->isLoggedIn($_SERVER['REMOTE_ADDR'], $core->auth->getCookie('pp_a
 	/*
 	 * Set Language in Database (& Set Cookie)
 	 */
-	if(file_exists('../../src/framework/lang/'.$_GET['language'].'.json')){
-	
-		$updateLanguage = $mysql->prepare("UPDATE `users` SET `language` = :language WHERE `id` = :id");
-		$updateLanguage->execute(array(
-			':language' => $_GET['language'],
-			':id' => $core->user->getData('id')
-		));
-		
-		setcookie("pp_language", $_GET['language'], time() + 2678400, '/', $core->settings->get('cookie_website'));
-	
+	if(file_exists(SRC_DIR.'lang/'.$_GET['language'].'.json')){
+
+		$account = ORM::forTable('users')->findOne($core->user->getData('id'));
+        $account->set(array(
+            'language' => $_GET['language']
+        ));
+        $account->save();
+
+		setcookie("pp_language", $_GET['language'], time() + 2678400, '/');
+
 	}
-		
+
 }else{
 
 	/*
 	 * Set Language Cookie for One Month
 	 */
-	if(file_exists('../../src/framework/lang/'.$_GET['language'].'.json'))
-		setcookie("pp_language", $_GET['language'], time() + 2678400, '/', $core->settings->get('cookie_website'));
-		
+	if(file_exists(SRC_DIR.'lang/'.$_GET['language'].'.json'))
+		setcookie("pp_language", $_GET['language'], time() + 2678400, '/');
+
 }
 
 /*
