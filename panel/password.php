75f4d4cb86984bd5196a74c392e822fc1f075b95
Lord_Ralex <lordralex@gmail.com>
2014-11-18 16:40:51 -0600

    Fix license for affected files

--- panel/password.php
+++ panel/password.php
@@ -2,7 +2,7 @@
 
 /*
 	PufferPanel - A Minecraft Server Management Panel
-	Copyright (c) 2014 PufferPanel
+	Copyright (c) 2014 Dane Everitt
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
