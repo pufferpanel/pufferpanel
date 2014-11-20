dfe554f12a6af4fe4c5e9be44e0d538b9e79193b
Lord_Ralex <lordralex@gmail.com>
2014-11-19 22:02:30 -0600

    Complete initial klein systems - At this time, the files that have been converted are loaded - Login is confirmed to work for invalid + valid data, reset view works, however fails on post due to other issues

--- panel/router.php
+++ panel/router.php
@@ -24,6 +24,8 @@ session_start();
 
 require_once('../src/core/core.php');
 
+use \ORM as ORM;
+
 $klein = new \Klein\Klein();
 
 $klein->respond('GET', '@!\.(php)', function($request, $response) {
@@ -52,10 +54,10 @@ $klein->respond(function($request, $response, $service, $app) use ($core, $twig,
     });
 });
 
-$klein->respond('!(logout|index|register|password)', function($request, $response, $service, $app) {
-    if (!$app->loggedIn) {
+$klein->respond('/!(logout|index|register|password)', function($request, $response, $service, $app) {
+    if (!$app->isLoggedIn) {
         $response->redirect('/index')->send();
-        throw new Exception("Not logged in");
+        throw new \Exception("Not logged in");
     }
 });
 
@@ -82,20 +84,19 @@ $klein->respond('GET', '/logout', function($request, $response, $service, $app) 
 
 $klein->with('/account', 'account.php');
 $klein->with('/password', 'password.php');
-$klein->with('/index', 'index.php');
 
 $klein->respond('GET', '/', function($request, $response) use ($core) {
-	if($core->auth->isLoggedIn($request->server()['REMOTE_ADDR'], $core->auth->getCookie('pp_auth_token')) === true) {
+	/*if($core->auth->isLoggedIn($request->server()['REMOTE_ADDR'], $core->auth->getCookie('pp_auth_token')) === true) {
 		$response->redirect('/servers', 302)->send();
-	}
+	}*/
 });
 
-$klein->respond('GET', '/', function($request, $response) use ($core, $twig, $pageStartTime) {
-	if($response->isSent()) {
+$klein->respond('GET', '/[index]', function($request, $response) use ($core, $twig, $pageStartTime) {
+	/*if($response->isSent()) {
 		return;
-	}
+	}*/
 
-	echo $twig->render('panel/index.html', array(
+	return $twig->render('panel/index.html', array(
 		'xsrf' => $core->auth->XSRF(),
 		'footer' => array(
 			'seconds' => number_format((microtime(true) - $pageStartTime), 4)
@@ -103,14 +104,116 @@ $klein->respond('GET', '/', function($request, $response) use ($core, $twig, $pa
 	));
 });
 
+$klein->respond('POST', '/index', function($request, $response) use ($core) {
+	if($response->isSent()) {
+		return;
+	}
 
-$klein->onError(function ($klein, $err) {
+	if($request->param('totp') !== null && $request->param('check') !== null) {
+
+		if(empty($request->param('totp')) || empty($request->param('check'))) {
+			echo false;
+		} else {
+
+			$totp = ORM::forTable('users')->select('use_totp')->where('email', $request->param('check'))->findOne();
+
+			if($totp === false) {
+				echo false;
+			} else {
+				echo ($totp->use_totp == 1) ? true : false;
+			}
+
+		}
+
+	} else if($request->param('do') == 'login') {
+
+		/* XSRF Check */
+		if($core->auth->XSRF($request->param('xsrf')) !== true) {
+			$response->redirect('/index?error=token', 302)->send();
+			return;
+		}
+
+
+		/*
+		 * Get the Account Details
+		 */
+		$account = ORM::forTable('users')->where('email', $request->param('email'))->findOne();
+
+		if($core->auth->verifyPassword($request->param('email'), $request->param('password'))) {
+
+			/*
+			 * Validate TOTP Key
+			 */
+			if($account->use_totp == 1) {
+
+				if(!$core->auth->validateTOTP($request->param('totp_token'), $account->totp_secret)) {
+					$core->log->getUrl()->addLog(0, 1, array('auth.account_login_fail_totp', 'A failed attempt to login to the account was made from ' . $request->server()['REMOTE_ADDR'] . '. The login failed due to TOTP 2FA mismatch.'));
+					$response->redirect('/index?totp=error', 302)->send();
+					return;
+				}
+
+			}
+
+			/*
+			 * Account Exists
+			 * Set Cookies and List Servers
+			 */
+			$token = $core->auth->keygen('12');
+			$expires = ($request->param('remember_me') === null) ? (time() + 604800) : null;
+
+			setcookie("pp_auth_token", $token, $expires, '/');
+
+			$account->set(array(
+				'session_id' => $token,
+				'session_ip' => $request->server()['REMOTE_ADDR']
+			));
+			$account->save();
+
+			if($account->notify_login_s == 1) {
+
+				$core->email->generateLoginNotification('success', array(
+					'IP_ADDRESS' => $request->server()['REMOTE_ADDR'],
+					'GETHOSTBY_IP_ADDRESS' => gethostbyaddr($request->server()['REMOTE_ADDR'])
+				))->dispatch($request->param('email'), $core->settings->get('company_name') . ' - Account Login Notification');
+
+			}
+
+			$core->log->getUrl()->addLog(0, 1, array('auth.account_login', 'Account was logged in from ' . $request->server()['REMOTE_ADDR'] . '.', $account->id));
+			$response->redirect('/servers', 302)->send();
+
+		} else {
+
+			if($account !== false) {
+
+				if($account->notify_login_f == 1) {
+
+					/*
+					 * Send Email
+					 */
+					$core->email->generateLoginNotification('failed', array(
+						'IP_ADDRESS' => $request->server()['REMOTE_ADDR'],
+						'GETHOSTBY_IP_ADDRESS' => gethostbyaddr($request->server()['REMOTE_ADDR'])
+					))->dispatch($request->param('email'), $core->settings->get('company_name') . ' - Account Login Failure Notification');
+
+				}
+
+			}
+
+			$core->log->getUrl()->addLog(0, 1, array('auth.account_login_fail', 'A failed attempt to login to the account was made from ' . $request->server()['REMOTE_ADDR'] . '.'));
+			$response->redirect('/index?error=true', 302)->send();
+
+		}
+	}
+});
+
+
+/*$klein->onError(function ($klein, $err) {
 	if($err !== "Not logged in" && !$klein->response()->isSent()) {
 
 		//fatal error occurred somewhere, logging
 		error_log($err);
 
 	}
-});
+});*/
 
 $klein->dispatch();
