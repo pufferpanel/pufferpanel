8cabe9ff01064db7882d9f505e6a05dd2875991a
Lord_Ralex <lordralex@gmail.com>
2014-12-08 17:55:18 -0600

    Add node deletion, resolves #333 Only allows for deleting a node when it has no servers on it

---
+++ panel/admin/node/ajax/delete/delete.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+  PufferPanel - A Minecraft Server Management Panel
+  Copyright (c) 2014 Dane Everitt
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+
+namespace PufferPanel\Core;
+
+use \ORM as ORM;
+
+require_once('../../../../../src/core/core.php');
+
+if ($core->auth->isLoggedIn($_SERVER['REMOTE_ADDR'], $core->auth->getCookie('pp_auth_token'), null, true) !== true) {
+	Components\Page::redirect('../../../index.php');
+}
+
+if(!isset($_POST['node']) || empty($_POST['node'])) {
+	Components\Page::redirect('../../delete.php?node=unknown');
+}
+
+$node = ORM::forTable('nodes')
+		->selectMany('nodes.id', 'nodes.node')
+		->left_outer_join('servers', array('nodes.id', '=', 'servers.node'))
+		->where_null('servers.id')
+		->where('nodes.id', $_POST['node'])
+		->findOne();
+
+if(!$node) {
+	Components\Page::redirect('../../delete.php?node=notempty');
+}
+
+$node->delete();
+
+Components\Page::redirect('../../list.php?node=deleted');
