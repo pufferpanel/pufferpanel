1607cee502d48662ed24171a6e87f5e12fd6fa3b
Dane Everitt <DaneEveritt@users.noreply.github.com>
2014-12-12 21:29:15 -0500

    Actually update the server IP and port through GSD

--- panel/admin/server/ajax/server/connection.php
+++ panel/admin/server/ajax/server/connection.php
@@ -17,7 +17,7 @@
     along with this program.  If not, see http://www.gnu.org/licenses/.
  */
 namespace PufferPanel\Core;
-use \ORM as ORM;
+use \ORM, \Unirest;
 
 require_once('../../../../../src/core/core.php');
 
@@ -39,14 +39,39 @@ $core->user->rebuildData($core->server->getData('owner_id'));
 $ports = json_decode($core->server->nodeData('ports'), true);
 $ips = json_decode($core->server->nodeData('ips'), true);
 
-if(!array_key_exists($_POST['server_ip'], $ports))
+if(!array_key_exists($_POST['server_ip'], $ports)) {
 	Components\Page::redirect('../../view.php?id='.$_POST['sid'].'&error=server_ip&disp=no_ip');
+}
 
-if(!array_key_exists($_POST['server_port'], $ports[$_POST['server_ip']]))
+if(!array_key_exists($_POST['server_port'], $ports[$_POST['server_ip']])) {
 	Components\Page::redirect('../../view.php?id='.$_POST['sid'].'&error=server_port&disp=no_port');
+}
 
-if($ports[$_POST['server_ip']][$_POST['server_port']] == 0 && $_POST['server_port'] != $core->server->getData('server_port'))
+if($ports[$_POST['server_ip']][$_POST['server_port']] == 0 && $_POST['server_port'] != $core->server->getData('server_port')) {
 	Components\Page::redirect('../../view.php?id='.$_POST['sid'].'&error=server_port&disp=port_in_use');
+}
+
+if(!$core->gsd->avaliable($core->server->nodeData('ip'), $core->server->nodeData('gsd_listen'))) {
+	Components\Page::redirect('../../view.php?id='.$_POST['sid'].'&disp=o_fail');
+}
+
+$request = Unirest::put(
+	"http://".$core->server->nodeData('ip').":".$core->server->nodeData('gsd_listen')."/gameservers/".$core->server->getData('gsd_id'),
+	array(
+		'X-Access-Token' => $core->server->nodeData('gsd_secret')
+	),
+	array(
+		"game_cfg" => json_encode(array(
+			"gameport" => (int) $_POST['server_port'],
+			"gamehost" => $_POST['server_ip']
+		))
+	)
+);
+
+if($request->code > 204){
+	error_log($request->raw_body, 0);
+	Components\Page::redirect('../../view.php?id='.$_POST['sid'].'&disp=gsd_error');
+}
 
 $server = ORM::forTable('servers')->findOne($_POST['sid']);
 $server->server_ip = $_POST['server_ip'];
@@ -69,26 +94,6 @@ $node = ORM::forTable('nodes')->findOne($_POST['nid']);
 $node->ports = json_encode($ports);
 $node->ips = json_encode($ips);
 $node->save();
-
-/*
-* Build the Data
-*/
-$url = "http://".$core->server->nodeData('ip').":".$core->server->nodeData('gsd_listen')."/gameservers/".$core->server->getData('gsd_id');
-$data = json_encode(array(
-	"gameport" => (int)$_POST['server_port'],
-	"gamehost" => $_POST['server_ip']
-));
-
-/*
-* Run Query Aganist GSD
-*/
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
-curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-	'X-Access-Token: '.$core->server->nodeData('gsd_secret')
-));
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
 
 Components\Page::redirect('../../view.php?id='.$_POST['sid']);
 
