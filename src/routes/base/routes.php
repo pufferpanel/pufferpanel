d8cd6c8f2afcad963d039663c6ace5012e375aa2
Dane Everitt <DaneEveritt@users.noreply.github.com>
2014-12-24 15:50:28 -0500

    Add support for login page

--- src/routes/base/routes.php
+++ src/routes/base/routes.php
@@ -16,11 +16,113 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see http://www.gnu.org/licenses/.
 */
+use PufferPanel\Core, \ORM;
+
+$klein->respond('GET', '/', function($request, $response, $service, $app) {
+
+	if($response->isSent()) {
+		return;
+	}
+
+	if($app->isLoggedIn) {
+		$response->redirect('/servers', 302)->send();
+	} else {
+		$response->redirect('/index')->send();
+	}
+
+});
 
 $klein->respond('GET', '/account', function() use ($core) {
 });
 
 $klein->respond('POST', '/account', function() use ($core) {
+});
+
+$klein->with('/index', function() use ($klein, $core) {
+
+	$klein->respond('GET', '', function($request, $response, $service) use ($core) {
+
+		$response->body($core->twig->render('panel/index.html', array(
+			'xsrf' => $core->auth->XSRF(),
+			'flash' => $service->flashes()
+		)));
+
+	});
+
+	$klein->respond('POST', '', function($request, $response, $service) use ($core) {
+
+		if($core->auth->XSRF($request->param('xsrf')) !== true) {
+
+			$service->flash('<div class="alert alert-warning">The XSRF token recieved was not valid. Please make sure cookies are enabled and try your request again.</div>');
+			$response->redirect('/index')->send();
+
+		}
+
+		$account = ORM::forTable('users')->where('email', $request->param('email'))->findOne();
+
+		if(!$core->auth->verifyPassword($request->param('email'), $request->param('password'))){
+
+			if($account && $account->notify_login_f == 1) {
+
+				$core->email->generateLoginNotification('failed', array(
+					'IP_ADDRESS' => $request->ip(),
+					'GETHOSTBY_IP_ADDRESS' => gethostbyaddr($request->ip())
+				))->dispatch($request->param('email'), $core->settings->get('company_name').' - Account Login Failure Notification');
+
+			}
+
+			$service->flash('<div class="alert alert-danger"><strong>Oh snap!</strong> The username or password you submitted was incorrect.</div>');
+			$response->redirect('/index')->send();
+
+		} else {
+
+			if($account->use_totp == 1 && !$core->auth->validateTOTP($request->param('totp_token'), $account->totp_secret)){
+				$service->flash('<div class="alert alert-danger"><strong>Oh snap!</strong> Your Two-Factor Authentication token was missing or incorrect.</div>');
+				$response->redirect('/index')->send();
+			}
+
+			$cookie = (object) array('token' => $core->auth->keygen('12'), 'expires' => ($request->param('remember_me')) ? (time() + 604800) : null);
+
+			$account->set(array(
+				'session_id' => $cookie->token,
+				'session_ip' => $request->ip()
+			));
+			$account->save();
+
+			if($account->notify_login_s == 1){
+
+				$core->email->generateLoginNotification('success', array(
+					'IP_ADDRESS' => $request->ip(),
+					'GETHOSTBY_IP_ADDRESS' => gethostbyaddr($request->ip())
+				))->dispatch($request->param('email'), $core->settings->get('company_name').' - Account Login Notification');
+
+			}
+
+			$response->cookie('pp_auth_token', $cookie->token, $cookie->expires);
+			$response->redirect('/servers')->send();
+
+		}
+
+	});
+
+	$klein->respond('POST', '/totp', function($request, $response) use ($core) {
+
+		if(!$request->param('totp') || !$request->param('check')) {
+			$response->body(false);
+		} else {
+
+			$totp = ORM::forTable('users')->select('use_totp')->where('email', $request->param('check'))->findOne();
+
+			if(!$totp) {
+				$response->body(false);
+			} else {
+				$response->body(($totp->use_totp == 1) ? true : false);
+			}
+
+		}
+
+	});
+
 });
 
 $klein->respond('GET', '/logout', function() use ($core) {
