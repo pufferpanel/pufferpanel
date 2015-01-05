4ec5256abd42f7a28ca1dc071a819a730ea33786
Dane Everitt <DaneEveritt@users.noreply.github.com>
2015-01-04 20:20:01 -0500

    Fix the query class to remove errors and use Unirest

--- src/core/query.php
+++ src/core/query.php
@@ -28,6 +28,12 @@ class Query {
 
 	protected $server = false;
 
+	protected $node = false;
+
+	protected $query = false;
+
+	protected $query_data;
+
 	/**
 	 * Builds server data using a specified ID, Hash, and Root Administrator Status.
 	 *
@@ -36,16 +42,12 @@ class Query {
 	 */
 	public function __construct($serverid) {
 
-		if($serverid === false) {
-			$this->_queryData = false;
-		} else {
-
-			$this->gsid = (int)$serverid;
+		if(is_numeric($serverid)) {
 
 			/*
 			 * Load Information into Script
 			 */
-			$this->server = ORM::forTable('servers')->findOne($this->gsid);
+			$this->server = ORM::forTable('servers')->findOne($serverid);
 
 			/*
 			 * Load Node Information into Script
@@ -66,24 +68,21 @@ class Query {
 	 */
 	public function check_status($ip, $port, $id, $secret){
 
-		$this->context = stream_context_create(array(
-			"http" => array(
-				"method" => "GET",
-				"timeout" => 1,
-				"header" => "X-Access-Token: ".$secret
-			)
-		));
-		$this->gatherData = @file_get_contents("http://".$ip.":".$port."/gameservers/".$id , 0, $this->context);
+		try {
 
-		$this->raw = json_decode($this->gatherData, true);
+			Unirest::timeout(1);
+			$request = Unirest::get(
+				"http://".$ip.":".$port."/gameservers/".$id,
+				array(
+					'X-Access-Token' => $secret
+				)
+			);
 
-		if(!$this->gatherData) {
-			return false;
-		} else if(json_last_error() == JSON_ERROR_NONE) {
-			return $this->raw['status'] != 0;
-		} else {
+		} catch(\Exception $e) {
 			return false;
 		}
+
+		return (isset($request->body->status)) ? $request->body->status : false;
 
 	}
 
@@ -95,43 +94,40 @@ class Query {
 	 */
 	public function online($override = false) {
 
-		if($this->server === false) {
+		if(!$this->server || !$this->node) {
 			return false;
 		} else {
 
-			$this->server->gsd_id = ($override !== false) ? (int)$override : $this->server->gsd_id;
+			$this->server->gsd_id = (!$override) ?: (int) $override;
 
-			$this->context = stream_context_create(array(
-				"http" => array(
-					"method" => "GET",
-					"timeout" => 1,
-					"header" => "X-Access-Token: ".$this->node->gsd_secret
-				)
-			));
-			$this->gatherData = file_get_contents("http://".$this->node->ip.":".$this->node->gsd_listen."/gameservers/".$this->server->gsd_id , 0, $this->context);
+			try {
 
-			$this->raw = json_decode($this->gatherData, true);
+				Unirest::timeout(1);
+				$request = Unirest::get(
+					"http://".$this->node->ip.":".$this->node->port."/gameservers/".$this->server->gsd_id,
+					array(
+						'X-Access-Token' => $this->node->gsd_secret
+					)
+				);
 
-			/*
-				 * Valid Data was Returned
-				 */
-			if(!$this->gatherData) {
-				return false;
-			} else if(json_last_error() == JSON_ERROR_NONE) {
-
-				if($this->raw['status'] == 0) {
-					return false;
-				} else {
-
-					$this->_jsonData = (array_key_exists('query', $this->raw)) ? $this->raw['query'] : null;
-					$this->_serverPID = $this->raw['pid'];
-					$this->_jsonProcess = (array_key_exists('process', $this->raw)) ? $this->raw['process'] : array();
-					return true;
-					
-				}
-			} else {
+			} catch(\Exception $e) {
 				return false;
 			}
+
+			/*
+			 * Valid Data was Returned
+			 */
+			if(!isset($request->body->status) || $request->body->status == 0) {
+				return false;
+			} else {
+
+				$this->query_data['query'] = (isset($request->body->query)) ? $request->body->query : null;
+				$this->query_data['pid'] = (isset($request->body->pid)) ? $request->body->pid : null;
+				$this->query_data['process'] = (isset($request->body->pid)) ? $request->body->pid : [];
+				return true;
+
+			}
+
 		}
 
 	}
@@ -143,29 +139,7 @@ class Query {
 	 */
 	public function pid() {
 
-		return $this->online() ? $this->_serverPID : null;
-
-	}
-
-	/**
-	 * Returns process information for the last server query.
-	 *
-	 * @param string $element A specific part of the JSON to return, if not provided the entire array is returned.
-	 * @return mixed
-	 */
-	public function retrieve_process($element = null) {
-
-		if($this->online()) {
-
-			if(is_null($element)) {
-				return $this->_jsonProcess;
-			} else {
-				return (array_key_exists($element, $this->_jsonProcess)) ? $this->_jsonProcess[$element] : null;
-			}
-			
-		} else {
-			return null;
-		}
+		return ($this->online()) ? $this->query_data['pid'] : null;
 
 	}
 
@@ -177,12 +151,12 @@ class Query {
 	*/
 	public function retrieve($element = null) {
 
-		if($this->online() === true) {
-			
+		if($this->online()) {
+
 			if(is_null($element)) {
-				return $this->_jsonData;
+				return $this->query_data;
 			} else {
-				return (array_key_exists($element, $this->_jsonData)) ? $this->_jsonData[$element] : null;
+				return (array_key_exists($element, $this->query_data)) ? $this->query_data[$element] : null;
 			}
 
 		} else {
@@ -213,14 +187,14 @@ class Query {
 			} catch(\Exception $e) {
 
 				\Tracy\Debugger::log($e);
-				return array("Daemon error occured.");
+				return "Daemon error occured.";
 
 			}
 
 			return $response->body;
 
 		} else {
-			return array('Server is currently offline.');
+			return "Server is currently offline.";
 		}
 
 	}
