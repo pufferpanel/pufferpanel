20a4cd09961f905dc21e4e307cc5600a65cbc308
Lord_Ralex <lordralex@gmail.com>
2015-01-07 14:07:37 -0600

    Fix load order

--- src/core/autoloader.php
+++ src/core/autoloader.php
@@ -50,7 +50,7 @@ require_once(SRC_DIR.'core/files.php');
 require_once(SRC_DIR.'core/user.php');
 require_once(SRC_DIR.'core/language.php');
 require_once(SRC_DIR.'core/log.php');
-require_once(SRC_DIR.'core/query.php');
 require_once(SRC_DIR.'core/server.php');
+require_once(SRC_DIR.'core/query.php');
 require_once(SRC_DIR.'core/settings.php');
 require_once(SRC_DIR.'core/routes.php');
