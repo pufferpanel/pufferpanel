02f5ddcb82de5d0fb4631ff1ec8c93127b7e4b5b
Dane Everitt <DaneEveritt@users.noreply.github.com>
2015-02-02 16:51:58 -0500

    Complete implementation of server adding through Klein

--- src/routes/admin/server/routes.php
+++ src/routes/admin/server/routes.php
@@ -17,13 +17,7 @@
     along with this program.  If not, see http://www.gnu.org/licenses/.
 */
 namespace PufferPanel\Core;
-use \ORM, \Tracy\Debugger, \Unirest\Request;
-
-$klein->respond('GET', '/admin/server/add', function($request, $response, $service) use ($core) {
-
-	$response->body($core->twig->render('admin/server/add.html'))->send();
-
-});
+use \ORM, \Tracy\Debugger, \Unirest\Request, \PufferPanel\Core\Components\Functions;
 
 $klein->respond('GET', '/admin/server', function($request, $response, $service) use ($core) {
 
@@ -312,5 +306,224 @@ $klein->respond('POST', '/admin/server/view/[i:id]/settings', function($request,
 		return;
 
 	}
+
+});
+
+$klein->respond('GET', '/admin/server/new', function($request, $response, $service) use ($core) {
+
+	$response->body($core->twig->render(
+		'admin/server/new.html',
+		array(
+			'locations' => ORM::forTable('locations')->findMany(),
+			'flash' => $service->flashes()
+		)
+	))->send();
+
+});
+
+$klein->respond('POST', '/admin/server/new', function($request, $response, $service) use($core) {
+
+	$server_port = $request->param['server_port_'.str_replace('.', '_', $request->param('server_ip'))];
+
+	$node = ORM::forTable('nodes')->findOne($request->param('node'));
+
+	if(!$node) {
+
+		$service->flash('<div class="alert alert-danger">The selected node does not exist on the system.</div>');
+		$response->redirect('/admin/server/new')->send();
+		return;
+
+	}
+
+	if(!preg_match('/^[\w-]{4,35}$/', $request->param('server_name'))) {
+
+		$service->flash('<div class="alert alert-danger">The name provided for the server did not meet server requirements. Server names must be between 4 and 3 characters long and contain no special characters.</div>');
+		$response->redirect('/admin/server/new')->send();
+		return;
+
+	}
+
+	$ips = json_decode($node->ips, true);
+	$ports = json_decode($node->ports, true);
+
+	if(
+		!array_key_exists($request->param('server_ip'), $ips) ||
+		!array_key_exists($server_port, $ports[$request->param('server_ip')]) ||
+		$ports[$request->param('server_ip')][$server_port] == 0
+	) {
+
+		$service->flash('<div class="alert alert-danger">The selected IP or Port is currently in use or not avaliable.</div>');
+		$response->redirect('/admin/server/new')->send();
+		return;
+
+	}
+
+	$user = ORM::forTable('users')->select('id')->where('email', $request->param('email'))->findOne();
+
+	if(!$user) {
+
+		$service->flash('<div class="alert alert-danger">The email provided does not match any account in the system.</div>');
+		$response->redirect('/admin/server/new')->send();
+		return;
+
+	}
+
+	/*
+	* Validate Disk & Memory
+	*/
+	if(
+		!is_numeric($request->param('alloc_mem')) ||
+		!is_numeric($request->param('alloc_disk')) ||
+		!is_numeric($request->param('cpu_limit'))
+	) {
+
+		$service->flash('<div class="alert alert-danger">Allocated memory, disk, and CPU must all be integers.</div>');
+		$response->redirect('/admin/server/new')->send();
+		return;
+
+	}
+
+	if($request->param('ftp_pass') != $request->param('ftp_pass_2') || !$core->auth->validatePasswordRequirements($request->param('ftp_pass'))) {
+
+		$service->flash('<div class="alert alert-danger">The FTP password provided did not match in both fields or did not meet the server password requirements.</div>');
+		$response->redirect('/admin/server/new')->send();
+		return;
+
+	}
+
+	$iv = $core->auth->generate_iv();
+	$ftp_password = $core->auth->encrypt($request->param('ftp_pass'), $iv);
+	$ftp_username = Functions::generateFTPUsername($request->param('server_name'));
+	$server_hash = $core->auth->generateUniqueUUID('servers', 'hash');
+	$gsd_secret = $core->auth->generateUniqueUUID('servers', 'gsd_secret');
+
+	/*
+	* Build Call
+	*/
+	$data = array(
+		"name" => $server_hash,
+		"user" => $ftp_username,
+		"overide_command_line" => "",
+		"path" => $node->gsd_server_dir.$ftp_username,
+		"build" => array(
+			"install_dir" => '/mnt/MC/CraftBukkit/',
+			"disk" => array(
+				"hard" => ($request->param('alloc_disk') < 32) ? 32 : (int) $request->param('alloc_disk'),
+				"soft" => ($request->param('alloc_disk') > 2048) ? (int) $request->param('alloc_disk') - 1024 : 32
+			),
+			"cpu" => (int) $request->param('cpu_limit')
+		),
+		"variables" => array(
+			"-jar" => "server.jar",
+			"-Xmx" => $request->param('alloc_mem')."M"
+		),
+		"keys" => array(
+			$gsd_secret => array(
+				"s:ftp",
+				"s:get",
+				"s:power",
+				"s:files",
+				"s:files:get",
+				"s:files:delete",
+				"s:files:put",
+				"s:files:zip",
+				"s:query",
+				"s:console",
+				"s:console:send"
+			)
+		),
+		"gameport" => (int) $server_port,
+		"gamehost" => $request->param('server_ip'),
+		"plugin" => "minecraft",
+		"autoon" => false
+	);
+
+	try {
+
+		$unirest = Request::post(
+			'http://'.$node->ip.':'.$node->gsd_listen.'/gameservers',
+			array(
+				'X-Access-Token' => $node->gsd_secret
+			),
+			array(
+				'settings' => json_encode($data)
+			)
+		);
+
+		$server = ORM::forTable('servers')->create();
+		$server->set(array(
+			'gsd_id' => $unirest->body->id,
+			'hash' => $server_hash,
+			'gsd_secret' => $gsd_secret,
+			'encryption_iv' => $iv,
+			'node' => $request->param('node'),
+			'name' => $request->param('server_name'),
+			'modpack' => '0000-0000-0000-0',
+			'server_jar' => 'server.jar',
+			'owner_id' => $user->id,
+			'max_ram' => $request->param('alloc_mem'),
+			'disk_space' => $request->param('alloc_disk'),
+			'cpu_limit' => $request->param('cpu_limit'),
+			'date_added' => time(),
+			'server_ip' => $request->param('server_ip'),
+			'server_port' => $server_port,
+			'ftp_user' => $ftp_username,
+			'ftp_pass' => $ftp_password
+		));
+		$server->save();
+
+		$ips[$request->param('server_ip')]['ports_free']--;
+		$ports[$request->param('server_ip')][$server_port]--;
+
+		$node->ips = json_encode($ips);
+		$node->ports = json_encode($ports);
+		$node->save();
+
+		$core->email->buildEmail('admin_new_server', array(
+				'NAME' => $request->param('server_name'),
+				'FTP' => $node->fqdn.':21',
+				'MINECRAFT' => $node->fqdn.':'.$server_port,
+				'USER' => $ftpUser.'-'.$content['id'],
+				'PASS' => $request->param('ftp_pass')
+		))->dispatch($request->param('email'), Settings::config()->company_name.' - New Server Added');
+
+		$service->flash('<div class="alert alert-success">Server created successfully.</div>');
+		$response->redirect('/admin/server/view/'.$server->id())->send();
+		return;
+
+	} catch(\Exception $e) {
+
+		Debugger::log($e);
+		$service->flash('<div class="alert alert-danger">An error occured while trying to connect to the remote node. Please check that GSD is running and try again.</div>');
+		$response->redirect('/admin/server/new')->send();
+		return;
+
+	}
+
+});
+
+$klein->respond('POST', '/admin/server/new/node-list', function($request, $response) use($core) {
+
+	$response->body($core->twig->render(
+		'admin/server/node-list.html',
+		array(
+			'nodes' => ORM::forTable('nodes')->where('location', $request->param('location'))->findMany()
+		)
+	))->send();
+
+});
+
+$klein->respond('POST', '/admin/server/new/ip-list', function($request, $response) use($core) {
+
+	$node = ORM::forTable('nodes')->findOne($request->param('node'));
+	$node->ips = json_decode($node->ips, true);
+	$node->ports = json_decode($node->ports, true);
+
+	$response->body($core->twig->render(
+		'admin/server/ip-list.html',
+		array(
+			'node' => $node
+		)
+	))->send();
 
 });
