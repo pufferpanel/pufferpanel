397a801c2d694ff4507b9b25fd09471b99727330
Lord_Ralex <lordralex@gmail.com>
2015-02-10 23:07:34 -0600

    Fix use for accounts

--- src/core/routes/account.php
+++ src/core/routes/account.php
@@ -17,7 +17,7 @@
 	along with this program.  If not, see http://www.gnu.org/licenses/.
 */
 namespace PufferPanel\Core\Router;
-use \ORM;
+use \ORM, PufferPanel\Core\Settings;
 
 class Account extends \PufferPanel\Core\Email {
 
