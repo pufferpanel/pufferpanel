e329fae7fe89a805ce9beb58f0fbb344edaf7fcd
Dane Everitt <dane@daneeveritt.com>
2015-08-16 12:47:05 -0400

    Initial commit to Hapi.js

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * PufferPanel — Reinventing the way game servers are managed.
+ * Copyright (c) 2015 PufferPanel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+var Path = require('path')
+
+// Include HapiJS Routing Mechanisms
+require(Path.join(__dirname, 'lib/routes.js'))
