pufferpanel/pufferpanel

commit c4cb5dec49e75623531749b02a391d6a607381ab

Move user functions into user controller, also fix tests

lib/controllers/user.js
@@ -49,6 +49,27 @@ UserController.generateTOTP = function (request, next) {
  */
 
 /**
+ * Gets if a given user's TOTP option is enabled.
+ * If the user does not exist, then the callback will receive false.
+ *
+ * @param {String} email - Email of user
+ * @param {UserController~isTotpEnabledCb} next - Callback to handle response
+ */
+UserController.isTOTPEnabled = function (email, next) {
+
+    Users.select({ email: email }, function (err, user) {
+
+        return next(err || null, user !== undefined && user.use_totp === 1);
+    });
+};
+/**
+ * @callback UserController~isTotpEnabledCb
+ * @param {Error} err - Error if one occurred, otherwise null
+ * @param {Boolean} data - Whether or not the user's TOTP is enabled, or false if the user does not exist
+ */
+
+
+/**
  * Validates request to enable TOTP.
  * @param  {Object} request - Hapi request object
  * @param  {UserController~enableTotpCb} next - Callback to handle response

lib/controllers/authentication.js
@@ -17,6 +17,8 @@ var Users = Rfr('lib/models/users.js');
 var AuthenticationController = {};
 
 
+//TODO: Very likely this should be in UserController instead of here, as it's a user function
+//Or at least make a proxy function
 /**
  * Determines whether given credentials are valid.
  *
@@ -54,27 +56,6 @@ AuthenticationController.loginUser = function (email, password, totpToken, next)
  * @callback AuthenticationController~loginUserCb
  * @param {Error} err - Error if one occurred, otherwise null
  * @param {Object|String} data - The User who is now logged on, otherwise a String with the failure reason
- */
-
-
-/**
- * Gets if a given user's TOTP option is enabled.
- * If the user does not exist, then the callback will receive false.
- *
- * @param {String} email - Email of user
- * @param {Authentication~isTotpEnabledCb} next - Callback to handle response
- */
-AuthenticationController.isTOTPEnabled = function (email, next) {
-
-    Users.select({ email: email }, function (err, user) {
-
-        return next(err || null, user !== undefined && user.use_totp === 1);
-    });
-};
-/**
- * @callback AuthenticationController~isTotpEnabledCb
- * @param {Error} err - Error if one occurred, otherwise null
- * @param {Boolean} data - Whether or not the user's TOTP is enabled, or false if the user does not exist
  */
 
 

lib/routes/authentication.js
@@ -9,6 +9,7 @@
  */
 var Rfr = require('rfr');
 var Authentication = Rfr('lib/controllers/authentication.js');
+var User = Rfr('lib/controllers/user.js');
 var Logger = Rfr('lib/logger.js');
 
 var Routes = {
@@ -45,7 +46,7 @@ var Routes = {
         },
         totp: function (request, reply) {
 
-            Authentication.isTOTPEnabled(request.payload.check, function (err, data) {
+            User.isTOTPEnabled(request.payload.check, function (err, data) {
 
                 if (err !== null) {
                     Logger.error(err);

tests/authentication.js
@@ -15,64 +15,9 @@ Chai.config.includeStack = false;
 var Should = Chai.should();
 var Expect = Chai.expect;
 var Assert = Chai.assert;
-var Authentication = Rfr('server/auth.js');
+var Authentication = Rfr('lib/controllers/authentication.js');
 
-describe('Server/Auth', function () {
-
-  describe('validateLogin', function () {
-
-    context('when user logs in with valid credentials', function () {
-      it('should return successfully', function (done) {
-        Authentication.validateLogin({
-          payload: {
-            email: 'theoretical@email.com',
-            password: 'theoretically_correct_password'
-          }
-        }, function (reply) {
-          Expect(reply.error).to.be.undefined;
-          Expect(reply.success).to.be.defined;
-          Expect(reply.success).to.be.true;
-          done();
-        });
-      });
-    });
-
-    context('when user logs in with invalid credentials', function () {
-      it('should return unsuccessfully', function (done) {
-        Authentication.validateLogin({
-          payload: {
-            email: 'bad@email.com',
-            password: 'FalsePassword'
-          }
-        }, function (reply) {
-          Assert.isUndefined(reply.success, 'success is undefined');
-          Assert.isDefined(reply.error, 'error is defined');
-          done();
-        });
-      });
-    });
-
-  });
-
-  describe('updatePasswordHash', function () {
-
-    var oldPw = '$2y$10$CrEJiLF5OoK/D.FgBs8Wc.Kr0C0KZaxWwOJwlYI4P98wjHP9BzXnK';
-    var newPw = '$2a$10$CrEJiLF5OoK/D.FgBs8Wc.Kr0C0KZaxWwOJwlYI4P98wjHP9BzXnK';
-    var validatedPw = '$2a$10$CrEJiLF5OoK/D.FgBs8Wc.Kr0C0KZaxWwOJwlYI4P98wjHP9BzXnK';
-
-    context('when needs update', function () {
-      it('should update', function () {
-        Assert.strictEqual(Authentication.updatePasswordHash(oldPw), validatedPw, 'passwords are the same.');
-      });
-    });
-
-    context('when already converted', function () {
-      it('should not update', function () {
-        Assert.strictEqual(Authentication.updatePasswordHash(newPw), validatedPw, 'passwords are the same.');
-      });
-    });
-
-  });
+describe('Controller/Authentication', function () {
 
   describe('generatePasswordHash', function () {
 

tests/execute.sh
@@ -1,7 +1,7 @@
 npm run-script test-eslint
 eslintRes=$?
-#npm run-script test-mocha
-#mochaRes=$?
+npm run-script test-mocha
+mochaRes=$?
 mochaRes=0
 if [ $mochaRes -ne 0 ] || [ $eslintRes -ne 0 ] ;
 then

commit 03b9b295992d25e57f03786f2447bcc1b7e51129

Fixes bug introduced in c4cb5dec4

lib/controllers/user.js
@@ -57,7 +57,7 @@ UserController.generateTOTP = function (request, next) {
  */
 UserController.isTOTPEnabled = function (email, next) {
 
-    Users.select({ email: email }, function (err, user) {
+    UserModels.select({ email: email }, function (err, user) {
 
         return next(err || null, user !== undefined && user.use_totp === 1);
     });