ae34f09be591d5f7c32169f7d1e1dd2d22be26c8
Dane Everitt <dane@daneeveritt.com>
2015-11-16 23:18:07 -0500

    Fix tabs, make them spaces.

--- resources/views/base/index.blade.php
+++ resources/views/base/index.blade.php
@@ -4,29 +4,29 @@
 
 @section('content')
 <div class="col-md-9">
-	@if (Auth::user()->root_admin == 1)
-		<div class="alert alert-info">
-			You are viewing this server listing as an admin. As such, all servers installed on the system are displayed. Any servers that you are set as the owner of are marked with a blue dot to the left of their name.
-		</div>
-	@endif
-	@if (!is_null($servers))
-		<table class="table table-striped table-bordered table-hover">
-			<thead>
-				<tr>
-					@if (Auth::user()->root_admin == 1)
+    @if (Auth::user()->root_admin == 1)
+        <div class="alert alert-info">
+            You are viewing this server listing as an admin. As such, all servers installed on the system are displayed. Any servers that you are set as the owner of are marked with a blue dot to the left of their name.
+        </div>
+    @endif
+    @if (!is_null($servers))
+        <table class="table table-striped table-bordered table-hover">
+            <thead>
+                <tr>
+                    @if (Auth::user()->root_admin == 1)
                         <th></th>
                     @endif
-					<th>Server Name</th>
-					<th>Location</th>
-					<th>Node</th>
-					<th>Connection</th>
-					<th></th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach ($servers as $server)
-					<tr class="dynUpdate" id="{{ $server->uuid }}">
-						@if (Auth::user()->root_admin == 1)
+                    <th>Server Name</th>
+                    <th>Location</th>
+                    <th>Node</th>
+                    <th>Connection</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($servers as $server)
+                    <tr class="dynUpdate" id="{{ $server->uuid }}">
+                        @if (Auth::user()->root_admin == 1)
                             <td style="width:26px;">
                                 @if ($server->owner === Auth::user()->id)
                                     <i class="fa fa-circle" style="color:#008cba;"></i>
@@ -35,24 +35,24 @@
                                 @endif
                             </td>
                         @endif
-						<td><a href="/server/{{ $server->uuid }}">{{ $server->name }}</a></td>
-						<td>{{ $server->location }}</td>
-						<td>{{ $server->node }}</td>
-						<td><code>{{ $server->ip }}:{{ $server->port }}</code></td>
-						<td style="width:26px;"><i class="fa fa-circle-o-notch fa-spinner fa-spin applyUpdate"></i></td>
-					</tr>
-				@endforeach
-			</tbody>
-		</table>
-	@else
-		<div class="alert alert-info">
-			You do not currently have any servers listed on your account.
-		</div>
-	@endif
+                        <td><a href="/server/{{ $server->uuid }}">{{ $server->name }}</a></td>
+                        <td>{{ $server->location }}</td>
+                        <td>{{ $server->node }}</td>
+                        <td><code>{{ $server->ip }}:{{ $server->port }}</code></td>
+                        <td style="width:26px;"><i class="fa fa-circle-o-notch fa-spinner fa-spin applyUpdate"></i></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <div class="alert alert-info">
+            You do not currently have any servers listed on your account.
+        </div>
+    @endif
 </div>
 <script type="text/javascript">
 $(document).ready(function() {
-	$("#sidebar_links").find("a[href='/']").addClass('active');
+    $("#sidebar_links").find("a[href='/']").addClass('active');
 });
 </script>
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,91 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	@section('scripts')
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="robots" content="noindex">
-		<link rel="stylesheet" href="http://pffr.me/assets/css/bootstrap.css">
-		<link rel="stylesheet" href="http://pffr.me/assets/css/pufferpanel.css">
-		<link rel="stylesheet" href="http://pffr.me/assets/css/animate.css">
-		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
-		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-		<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/js/bootstrap.min.js"></script>
-		<script src="//cdnjs.cloudflare.com/ajax/libs/socket.io/1.3.7/socket.io.min.js"></script>
-		<script src="http://pffr.me/assets/javascript/admin.min.js"></script>
-		<script src="http://pffr.me/assets/javascript/bootstrap-notify.min.js"></script>
-	@show
-	<title>PufferPanel - @yield('title')</title>
+    @section('scripts')
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="robots" content="noindex">
+        <link rel="stylesheet" href="http://pffr.me/assets/css/bootstrap.css">
+        <link rel="stylesheet" href="http://pffr.me/assets/css/pufferpanel.css">
+        <link rel="stylesheet" href="http://pffr.me/assets/css/animate.css">
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/socket.io/1.3.7/socket.io.min.js"></script>
+        <script src="http://pffr.me/assets/javascript/admin.min.js"></script>
+        <script src="http://pffr.me/assets/javascript/bootstrap-notify.min.js"></script>
+    @show
+    <title>PufferPanel - @yield('title')</title>
 </head>
 <body>
-	<div class="container">
-		<div class="navbar navbar-default">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="/index">PufferPanel - Laravel</a>
-			</div>
-			<div class="navbar-collapse collapse navbar-responsive-collapse">
-				@section('server-name')
-					@if (isset($server->name))
-						<ul class="nav navbar-nav">
-							<li class="active" id="{{ $server->name }}"><a href="/server/{{ $server->id }}/index"><i id="applyUpdate" class="fa fa-circle-o-notch fa-spinner fa-spin spin-light"></i> {{ $server->name }}</a></li>
-						</ul>
-					@endif
-				@show
-				@section('right-nav')
-					<ul class="nav navbar-nav navbar-right">
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Langauge<b class="caret"></b></a>
-							<ul class="dropdown-menu">
-								<li><a href="/language/de">Deutsch</a></li>
-								<li><a href="/language/en">English</a></li>
-								<li><a href="/language/es">Espa&ntilde;ol</a></li>
-								<li><a href="/language/fr">Fran&ccedil;ais</a></li>
-								<li><a href="/language/it">Italiano</a></li>
-								<li><a href="/language/pl">Polski</a></li>
-								<li><a href="/language/pt">Portugu&ecirc;s</a></li>
-								<li><a href="/language/ru">&#1088;&#1091;&#1089;&#1089;&#1082;&#1080;&#1081;</a></li>
-								<li><a href="/language/se">Svenska</a></li>
-								<li><a href="/language/zh">&#20013;&#22269;&#30340;的</a></li>
-							</ul>
-						</li>
-						<li class="hidden-xs"><a href="/admin/index"><i class="fa fa-cogs"></i></a></li>
-						<li class="hidden-xs"><a href="/auth/logout"><i class="fa fa-power-off"></i></a></li>
-					</ul>
-				@show
-			</div>
-		</div>
-		<!-- Add Back Mobile Support -->
-		<div class="row">
-			<div class="col-md-3 hidden-xs hidden-sm" id="sidebar_links">
-				@section('sidebar')
-					<div class="list-group">
-						<a href="#" class="list-group-item list-group-item-heading"><strong>Account Controls</strong></a>
-						<a href="/account" class="list-group-item">Account Settings</a>
-						<a href="/totp" class="list-group-item">Account Security</a>
-						<a href="/" class="list-group-item">Your Servers</a>
-					</div>
-					@if (isset($server->name))
-						<div class="list-group">
-							<a href="#" class="list-group-item list-group-item-heading"><strong>Server Controls</strong></a>
-							<a href="/node/index" class="list-group-item">Server Overview</a>
-							<a href="/node/files" class="list-group-item">File Manager</a>
-							<a href="/node/users" class="list-group-item">Manage Sub-Users</a>
-							<a href="/node/settings" class="list-group-item">Manage Server</a>
-						</div>
-					@endif
-				@show
-			</div>
-			@yield('content')
-		</div>
-		<div class="footer">
-			<div class="row">
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="navbar navbar-default">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/index">PufferPanel - Laravel</a>
+            </div>
+            <div class="navbar-collapse collapse navbar-responsive-collapse">
+                @section('server-name')
+                    @if (isset($server->name))
+                        <ul class="nav navbar-nav">
+                            <li class="active" id="{{ $server->name }}"><a href="/server/{{ $server->id }}/index"><i id="applyUpdate" class="fa fa-circle-o-notch fa-spinner fa-spin spin-light"></i> {{ $server->name }}</a></li>
+                        </ul>
+                    @endif
+                @show
+                @section('right-nav')
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Langauge<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="/language/de">Deutsch</a></li>
+                                <li><a href="/language/en">English</a></li>
+                                <li><a href="/language/es">Espa&ntilde;ol</a></li>
+                                <li><a href="/language/fr">Fran&ccedil;ais</a></li>
+                                <li><a href="/language/it">Italiano</a></li>
+                                <li><a href="/language/pl">Polski</a></li>
+                                <li><a href="/language/pt">Portugu&ecirc;s</a></li>
+                                <li><a href="/language/ru">&#1088;&#1091;&#1089;&#1089;&#1082;&#1080;&#1081;</a></li>
+                                <li><a href="/language/se">Svenska</a></li>
+                                <li><a href="/language/zh">&#20013;&#22269;&#30340;的</a></li>
+                            </ul>
+                        </li>
+                        <li class="hidden-xs"><a href="/admin/index"><i class="fa fa-cogs"></i></a></li>
+                        <li class="hidden-xs"><a href="/auth/logout"><i class="fa fa-power-off"></i></a></li>
+                    </ul>
+                @show
+            </div>
+        </div>
+        <!-- Add Back Mobile Support -->
+        <div class="row">
+            <div class="col-md-3 hidden-xs hidden-sm" id="sidebar_links">
+                @section('sidebar')
+                    <div class="list-group">
+                        <a href="#" class="list-group-item list-group-item-heading"><strong>Account Controls</strong></a>
+                        <a href="/account" class="list-group-item">Account Settings</a>
+                        <a href="/totp" class="list-group-item">Account Security</a>
+                        <a href="/" class="list-group-item">Your Servers</a>
+                    </div>
+                    @if (isset($server->name))
+                        <div class="list-group">
+                            <a href="#" class="list-group-item list-group-item-heading"><strong>Server Controls</strong></a>
+                            <a href="/node/index" class="list-group-item">Server Overview</a>
+                            <a href="/node/files" class="list-group-item">File Manager</a>
+                            <a href="/node/users" class="list-group-item">Manage Sub-Users</a>
+                            <a href="/node/settings" class="list-group-item">Manage Server</a>
+                        </div>
+                    @endif
+                @show
+            </div>
+            @yield('content')
+        </div>
+        <div class="footer">
+            <div class="row">
+            </div>
+        </div>
+    </div>
 </body>
 </html>
