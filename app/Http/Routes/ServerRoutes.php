d7f0404c46563ba8df747e0ac063878bc2850e79
Samuel Ryan <sam@samryan.co.uk>
2015-12-06 19:04:09 +0000

    Reformats routes for readability

--- app/Http/Routes/ServerRoutes.php
+++ app/Http/Routes/ServerRoutes.php
@@ -8,18 +8,44 @@ class ServerRoutes {
 
     public function map(Router $router) {
         $router->group(['prefix' => 'server/{server}', 'middleware' => ['auth', 'server']], function ($server) use ($router) {
+            $router->get('/', [
+                'as' => 'server.index',
+                'uses' => 'Server\ServerController@getIndex'
+            ]);
 
-            $router->get('/', [ 'as' => 'server.index', 'uses' => 'Server\ServerController@getIndex' ]);
-            $router->get('/files', [ 'as' => 'files.index', 'uses' => 'Server\ServerController@getFiles' ]);
-            $router->get('/files/edit/{file}', [ 'as' => 'files.edit', 'uses' => 'Server\ServerController@getEditFile' ])->where('file', '.*');
-            $router->get('/files/download/{file}', [ 'as' => 'files.download', 'uses' => 'Server\ServerController@getDownloadFile' ])->where('file', '.*');
-            $router->get('/files/add', [ 'as' => 'files.add', 'uses' => 'Server\ServerController@getAddFile' ]);
+            $router->get('/files', [
+                'as' => 'files.index',
+                'uses' => 'Server\ServerController@getFiles'
+            ]);
+
+            $router->get('/files/edit/{file}', [
+                'as' => 'files.edit',
+                'uses' => 'Server\ServerController@getEditFile'
+            ])->where('file', '.*');
+
+            $router->get('/files/download/{file}', [
+                'as' => 'files.download',
+                'uses' => 'Server\ServerController@getDownloadFile'
+            ])->where('file', '.*');
+
+            $router->get('/files/add', [
+                'as' => 'files.add',
+                'uses' => 'Server\ServerController@getAddFile'
+            ]);
 
             // Ajax Routes
             $router->group(['prefix' => 'ajax'], function ($server) use ($router) {
-                $router->get('status', [ 'uses' => 'Server\AjaxController@getStatus' ]);
-                $router->post('files/directory-list', [ 'uses' => 'Server\AjaxController@postDirectoryList' ]);
-                $router->post('files/save', [ 'uses' => 'Server\AjaxController@postSaveFile' ]);
+                $router->get('status', [
+                    'uses' => 'Server\AjaxController@getStatus'
+                ]);
+
+                $router->post('files/directory-list', [
+                    'uses' => 'Server\AjaxController@postDirectoryList'
+                ]);
+
+                $router->post('files/save', [
+                    'uses' => 'Server\AjaxController@postSaveFile'
+                ]);
             });
         });
     }
