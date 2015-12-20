932b71cf5e401ed9cbf78f55dfa7b6c92f58da7f
Lord_Ralex <lordralex@gmail.com>
2015-12-20 14:58:15 -0600

    Fix base_dir references

--- public/router.php
+++ public/router.php
@@ -45,9 +45,9 @@ Twig_Autoloader::register();
  * enough to figure out if it is a local or development environment.
  */
 if(Config::config('debugging') === true) {
-	Debugger::enable(Debugger::DEVELOPMENT, BASEDIR.'/logs');
+	Debugger::enable(Debugger::DEVELOPMENT, BASE_DIR.'/logs');
 } else {
-	Debugger::enable(Debugger::DETECT, BASEDIR.'/logs');
+	Debugger::enable(Debugger::DETECT, BASE_DIR.'/logs');
 }
 Debugger::$strictMode = true;
 
