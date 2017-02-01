fc31a9a6a0ae9272c7473f7261b1ff51285f7680
Lord_Ralex <lordralex@gmail.com>
2017-01-31 21:28:33 -0600

    Begin subuser integration

--- src/core/routes/node/users.php
+++ src/core/routes/node/users.php
@@ -17,243 +17,275 @@
 	along with this program.  If not, see http://www.gnu.org/licenses/.
 */
 namespace PufferPanel\Core\Router\Node;
-use \ORM, \Unirest, \PufferPanel\Core\Settings;
 
-class Users extends \PufferPanel\Core\Email {
+use \ORM, \PufferPanel\Core\Settings;
 
-	use \PufferPanel\Core\Components\Authentication, \PufferPanel\Core\Components\Daemon, \PufferPanel\Core\Components\Error_Handler;
+class Users extends \PufferPanel\Core\Email
+{
 
-	/**
-	 * @param object
-	 */
-	protected $server;
+    use \PufferPanel\Core\Components\Authentication, \PufferPanel\Core\Components\Daemon, \PufferPanel\Core\Components\Error_Handler;
 
-	/**
-	 * Constructor class for \PufferPanel\Core\Router\Node\Users
-	 */
-	public function __construct(\PufferPanel\Core\Server $server) {
+    /**
+     * @param object
+     */
+    protected $server;
 
-		$this->server = $server;
+    /**
+     * Constructor class for \PufferPanel\Core\Router\Node\Users
+     */
+    public function __construct(\PufferPanel\Core\Server $server)
+    {
+        $this->server = $server;
+    }
 
-	}
+    /**
+     * Handles adding a new subuser to a server.
+     *
+     * @return bool
+     */
+    public function addSubuser(\Klein\DataCollection\DataCollection $data)
+    {
 
-	/**
-	 * Handles adding a new subuser to a server.
-	 *
-	 * @return bool
-	 */
-	public function addSubuser(\Klein\DataCollection\DataCollection $data) {
+        if (is_null($data->permissions) || !$data->permissions) {
+            $data->permissions = array('console.view');
+        }
 
-		if(is_null($data->permissions) || !$data->permissions) {
-			$data->permissions = array('console.view');
-		}
+        $registerToken = $this->keygen(32);
+        $data->permissions = self::_rebuildUserPermissions($data->permissions);
 
-		$registerToken = $this->keygen(32);
-		$daemon_permissionSecret = $this->generateUniqueUUID('servers', 'daemon_secret');
-		$data->permissions = self::_rebuildUserPermissions($data->permissions);
+        ORM::get_db()->beginTransaction();
 
-		ORM::get_db()->beginTransaction();
+        try {
 
-		try {
+            $user = ORM::forTable('users')->where('email', $data->email)->findOne();
 
-			$user = ORM::forTable('users')->where('email', $data->email)->findOne();
+            $checkUserExists = $user ? true : false;
+            if (!$checkUserExists) {
 
-			$checkUserExists = $user ? true : false;
-			if(!$checkUserExists) {
+                $user = ORM::forTable('users')->create()->set(array(
+                    'uuid' => $this->generateUniqueUUID('users', 'uuid'),
+                    'username' => null,
+                    'email' => $data->email,
+                    'password' => null,
+                    'language' => Settings::config('default_language'),
+                    'register_time' => time()
+                ));
+                $user->save();
 
-				$user = ORM::forTable('users')->create()->set(array(
-					'uuid' => $this->generateUniqueUUID('users', 'uuid'),
-					'username' => null,
-					'email' => $data->email,
-					'password' => null,
-					'language' => Settings::config('default_language'),
-					'register_time' => time()
-				));
-				$user->save();
+                ORM::forTable('account_change')->create()->set(array(
+                    'user_id' => $user->id,
+                    'type' => 'user_register',
+                    'content' => $data->email,
+                    'key' => $registerToken,
+                    'time' => time()
+                ))->save();
 
-				ORM::forTable('account_change')->create()->set(array(
-					'user_id' => $user->id,
-					'type' => 'user_register',
-					'content' => $data->email,
-					'key' => $registerToken,
-					'time' => time()
-				))->save();
+            }
 
-			}
+            ORM::forTable('subusers')->create()->set(array(
+                'user' => $user->id,
+                'server' => $this->server->getData('id'),
+                'daemon_permissions' => json_encode($data->permissions)
+            ))->save();
 
-			ORM::forTable('subusers')->create()->set(array(
-				'user' => $user->id,
-				'server' => $this->server->getData('id'),
-				'daemon_secret' => $daemon_permissionSecret,
-				'daemon_permissions' => json_encode(self::_buildDaemonPermissions($data->permissions))
-			))->save();
+            $email = null;
 
-			$email = null;
+            foreach ($data->permissions as $id => $permission) {
 
-			foreach(self::_rebuildUserPermissions($data->permissions) as $id => $permission) {
+                ORM::forTable('permissions')->create()->set(array(
+                    'user' => $user->id,
+                    'server' => $this->server->getData('id'),
+                    'permission' => $permission
+                ))->save();
 
-				ORM::forTable('permissions')->create()->set(array(
-					'user' => $user->id,
-					'server' => $this->server->getData('id'),
-					'permission' => $permission
-				))->save();
+            }
 
-			}
+            if ($checkUserExists) {
 
-			if($checkUserExists) {
+                $email = $this->buildEmail('new_subuser', array(
+                    'SERVER' => $this->server->getData('name'),
+                    'EMAIL' => $data->email
+                ));
 
-				$email = $this->buildEmail('new_subuser', array(
-					'SERVER' => $this->server->getData('name'),
-					'EMAIL' => $data->email
-				));
+            } else {
 
-			} else {
+                $email = $this->buildEmail('new_subuser_createaccount', array(
+                    'REGISTER_TOKEN' => $registerToken,
+                    'URLENCODE_TOKEN' => urlencode($registerToken),
+                    'SERVER' => $this->server->getData('name'),
+                    'EMAIL' => $data->email
+                ));
 
-				$email = $this->buildEmail('new_subuser_createaccount', array(
-					'REGISTER_TOKEN' => $registerToken,
-					'URLENCODE_TOKEN' => urlencode($registerToken),
-					'SERVER' => $this->server->getData('name'),
-					'EMAIL' => $data->email
-				));
+            }
 
-			}
+            $email->dispatch($data->email, Settings::config()->company_name . ' - You\'ve Been Invited to Manage a Server');
 
-			$email->dispatch($data->email, Settings::config()->company_name.' - You\'ve Been Invited to Manage a Server');
+            ORM::get_db()->commit();
+            return true;
 
-			ORM::get_db()->commit();
-			return true;
+        } catch (\Exception $e) {
 
-		} catch(\Exception $e) {
+            \Tracy\Debugger::log($e);
+            self::_setError("An error occurred when trying to update the server information.");
+            ORM::get_db()->rollBack();
+            return false;
 
-			\Tracy\Debugger::log($e);
-			self::_setError("An error occured when trying to update the server information.");
-			ORM::get_db()->rollBack();
-			return false;
+        }
 
-		}
+    }
 
-	}
+    /**
+     * Updates information for a subuser.
+     *
+     * @param array $data
+     * @return bool
+     */
+    public function modifySubuser(\Klein\DataCollection\DataCollection $data)
+    {
 
-	/**
-	 * Updates information for a subuser.
-	 *
-	 * @param array $data
-	 * @return bool
-	 */
-	public function modifySubuser(\Klein\DataCollection\DataCollection $data) {
+        $select = ORM::forTable('subusers')->where(array(
+            'user' => $data->user_id,
+            'server' => $this->server->getData('id')
+        ))->findOne();
 
-		$select = ORM::forTable('subusers')->where(array(
-			'user' => $data->user_id,
-			'server' => $this->server->getData('id')
-		))->findOne();
+        if (!$select) {
+            self::_setError("Invalid user was provided.");
+            return false;
+        }
 
-		if(!$select) {
-			self::_setError("Invalid user was provided.");
-			return false;
-		}
+        ORM::forTable('permissions')->where(array(
+            'user' => $data->user_id,
+            'server' => $this->server->getData('id')
+        ))->deleteMany();
 
-		ORM::forTable('permissions')->where(array(
-			'user' => $data->user_id,
-			'server' => $this->server->getData('id')
-		))->deleteMany();
-		
-		if(is_null($data->permissions) || !$data->permissions) {
-			$data->permissions = array('console.view');
-		}
+        $clientIds = ORM::forTable('oauth_clients')
+            ->where(array(
+                'user' => $data->user_id,
+                'server' => $this->server->getData('id')
+            ))->find_many();
 
-		foreach(self::_rebuildUserPermissions($data->permissions) as $id => $permission) {
+        foreach ($clientIds as $key => $client) {
+            ORM::forTable('oauth_access_tokens')
+                ->where('oauthClientId', $client->id)
+                ->deleteMany();
+            $client->delete();
+        }
 
-			ORM::forTable('permissions')->create()->set(array(
-				'user' => $select->user,
-				'server' => $this->server->getData('id'),
-				'permission' => $permission
-			))->save();
+        if (is_null($data->permissions) || !$data->permissions) {
+            $data->permissions = array('console.view');
+        }
 
-		}
+        $permissions = self::_rebuildUserPermissions($data->permissions);
 
-		$select->daemon_permissions = json_encode(self::_buildDaemonPermissions($data->permissions));
-		$select->save();
+        foreach ($permissions as $id => $permission) {
 
-	}
+            ORM::forTable('permissions')->create()->set(array(
+                'user' => $select->user,
+                'server' => $this->server->getData('id'),
+                'permission' => $permission
+            ))->save();
 
-	/**
-	 * Rebuilds user permissions to include the parent .view permission.
-	 *
-	 * @param array $data
-	 * @return array
-	 * @static
-	 */
-	protected final static function _rebuildUserPermissions(array $data) {
+        }
 
-		foreach($data as $permission) {
+    }
 
-			if(in_array($permission, array('files.edit', 'files.save', 'files.download', 'files.delete', 'files.create', 'files.upload', 'files.zip')) && !in_array('files.view', $data)) {
-				$data = array_merge($data, array("files.view"));
-			}
+    /**
+     * Rebuilds user permissions to include the parent .view permission.
+     *
+     * @param array $data
+     * @return array
+     * @static
+     */
+    protected final static function _rebuildUserPermissions(array $data)
+    {
 
-			if(in_array($permission, array('manage.rename.jar')) && !in_array('manage.rename.view', $data)) {
-				$data = array_merge($data, array("manage.rename.view"));
-			}
+        foreach ($data as $permission) {
 
-			if(in_array($permission, array('manage.ftp.details', 'manage.ftp.password')) && !in_array('manage.ftp.view', $data)) {
-				$data = array_merge($data, array("manage.ftp.view"));
-			}
+            if (in_array($permission, array('files.edit', 'files.save', 'files.download', 'files.delete', 'files.create', 'files.upload', 'files.zip')) && !in_array('files.view', $data)) {
+                $data = array_merge($data, array("files.view"));
+            }
 
-			if(in_array($permission, array('manage.ftp.details', 'manage.ftp.password', 'manage.rename.jar', 'manage.ftp.view', 'manage.rename.view')) && !in_array('manage.view', $data)) {
-				$data = array_merge($data, array("manage.view"));
-			}
+            if (in_array($permission, array('manage.rename.jar')) && !in_array('manage.rename.view', $data)) {
+                $data = array_merge($data, array("manage.rename.view"));
+            }
 
-		}
+            if (in_array($permission, array('manage.ftp.details', 'manage.ftp.password')) && !in_array('manage.ftp.view', $data)) {
+                $data = array_merge($data, array("manage.ftp.view"));
+            }
 
-		return $data;
+            if (in_array($permission, array('manage.ftp.details', 'manage.ftp.password', 'manage.rename.jar', 'manage.ftp.view', 'manage.rename.view')) && !in_array('manage.view', $data)) {
+                $data = array_merge($data, array("manage.view"));
+            }
 
-	}
+        }
 
-	/**
-	 * Revokes subuser permissions for a given user that has an active account on the panel.
-	 *
-	 * @param object $orm Database query object.
-	 * @return bool
-	 */
-	public function revokeActiveUserPermissions(ORM $orm) {
+        return $data;
 
-		if($orm->pending == 0) {
+    }
 
-			try {
+    /**
+     * Revokes subuser permissions for a given user that has an active account on the panel.
+     *
+     * @param object $orm Database query object.
+     * @return bool
+     */
+    public function revokeActiveUserPermissions(ORM $orm)
+    {
 
-                                ORM::forTable('permissions')
-					->where(array(
-						'user' => $orm->user,
-						'server' => $this->server->getData('id')
-					))->delete_many();
+        if ($orm->pending == 0) {
 
-				$orm->delete();
+            try {
+                ORM::forTable('permissions')
+                    ->where(array(
+                        'user' => $orm->user,
+                        'server' => $this->server->getData('id')
+                    ))->delete_many();
 
-				return true;
+                $clientIds = ORM::forTable('oauth_clients')
+                    ->where(array(
+                        'user' => $orm->user,
+                        'server' => $this->server->getData('id')
+                    ))->find_many();
 
-			} catch(\Exception $e) {
+                foreach ($clientIds as $key => $client) {
+                    ORM::forTable('oauth_access_tokens')
+                        ->where('oauthClientId', $client->id)
+                        ->deleteMany();
+                    $client->delete();
+                }
 
-				\Tracy\Debugger::log($e);
-				self::_setError("An error occured when trying to update the server.");
-				return false;
+                $orm->delete();
 
-			}
+                return true;
 
-		} else {
+            } catch (\Exception $e) {
 
-			ORM::forTable('account_change')->where(array(
-				'content' => $orm->email,
-				'type' => 'user_register'
-			))->findOne()->delete();
-			ORM::forTable('subusers')->where(array(
-				'id' => $orm->user,
-				'server' => $this->server->getData('id')
-			))->findOne()->delete();
-			$orm->delete();
-			return true;
+                \Tracy\Debugger::log($e);
+                self::_setError("An error occurred when trying to update the server.");
+                return false;
 
-		}
+            }
 
-	}
+        } else {
+
+            ORM::forTable('account_change')->where(array(
+                'content' => $orm->email,
+                'type' => 'user_register'
+            ))->findOne()->delete();
+            ORM::forTable('subusers')->where(array(
+                'id' => $orm->user,
+                'server' => $this->server->getData('id')
+            ))->findOne()->delete();
+            $orm->delete();
+            return true;
+
+        }
+
+    }
+
+    protected function __getDaemonPermissions(array $data)
+    {
+        return array();
+    }
 
 }
