975764b1689a5fda7873b50fd524fd46166f6a9d
Lord_Ralex <lordralex@gmail.com>
2017-02-25 16:04:18 -0600

    Lock down the oauth2 from a subuser's view

--- src/routes/node/routes.php
+++ src/routes/node/routes.php
@@ -22,11 +22,11 @@ namespace PufferPanel\Core;
 
 use ORM;
 
-$klein->respond('GET', '/node/[*]', function($request, $response) use ($core) {
+$klein->respond('GET', '/node/[*]', function ($request, $response) use ($core) {
     $response->cookie('accessToken', OAuthService::Get()->getAccessToken($core->user->getData('id'), $core->server->getData('id')));
 });
 
-$klein->respond('GET', '/node/index', function($request, $response, $service) use ($core) {
+$klein->respond('GET', '/node/index', function ($request, $response, $service) use ($core) {
 
     $protocol = "wss";
 
@@ -42,19 +42,19 @@ $klein->respond('GET', '/node/index', function($request, $response, $service) us
             'node' => $core->server->nodeData('node')
         )),
         'node' => array_merge($core->server->nodeData(),
-                array(
-                    'protocol' => $protocol
-                )),
+            array(
+                'protocol' => $protocol
+            )),
         'flash' => $service->flashes(),
         'user' => $core->user->getData(),
         'oauth' => OAuthService::Get()->getFor($core->user->getData('id'), $core->server->getData('id'))
     )));
 });
 
-$klein->respond('DELETE', '/node/oauth/[i:id]', function($request, $response, $service) use ($core) {
+$klein->respond('DELETE', '/node/oauth/[i:id]', function ($request, $response, $service) use ($core) {
     $service->validateParam('id')->isInt();
     $id = $request->param('id');
-    if(OAuthService::Get()->hasAccess($id, $core->user->getData('id'))) {
+    if (OAuthService::Get()->hasAccess($id, $core->user->getData('id'))) {
         OAuthService::Get()->revoke($id);
         $response->code(200);
         return;
@@ -62,22 +62,45 @@ $klein->respond('DELETE', '/node/oauth/[i:id]', function($request, $response, $s
     $response->code(401);
 });
 
-$klein->respond('POST', '/node/oauth', function($request, $response, $service) use ($core) {
+$klein->respond('POST', '/node/oauth', function ($request, $response, $service) use ($core) {
     $service->validateParam('oauthId')->notNull();
     $id = $request->param('oauthId');
     $name = $request->param('oauthName');
     $desc = $request->param('oauthDesc');
-    
+
+    if (strncmp($id, '.', 1) === 0) {
+        $service->flash('<div class="alert alert-danger">OAuth ids cannot start with \'.\'</div>');
+        $response->redirect('/node/index');
+        return;
+    }
+
+
     if ($name === 'undefined' || $name === '') {
         $name = $id;
     }
-    
+
     if ($desc === 'undefined' || $desc === '') {
         $desc = $name;
     }
-    
+
+    $scopeDb = ORM::for_table('oauth_clients')->where(array(
+            'user_id' => $core->user->getData('id'),
+            'server_id' => $core->server->getData('id'),
+            'client_id' => '.internal_' . $core->user->getData('id') . '_' . $core->server->getData('id'))
+    )->select('scopes')->find_one();
+
+    if (count($scopeDb) == 1) {
+        $scopes = $scopeDb['scopes'];
+    } else if ($core->server->getData('owner_id') == $core->user->getData('id')) {
+        $scopes = OAuthService::getUserScopes();
+    } else {
+        $service->flash('<div class="alert alert-danger">You are not the owner of the server nor have a defined OAuth setup, please contact your server owner to refresh your permissions</div>');
+        $response->redirect('/node/index');
+        return;
+    }
+
     $pdo = ORM::get_db();
-    $secret = OAuthService::Get()->create($pdo, $core->user->getData('id'), $core->server->getData('id'), $id, OAuthService::getUserScopes(), $name, $desc);
+    $secret = OAuthService::Get()->create($pdo, $core->user->getData('id'), $core->server->getData('id'), $id, $scopes, $name, $desc);
     $service->flash('<div class="alert alert-danger">Secret key generated: ' . $secret . '</div>');
 
     $response->redirect('/node/index');
