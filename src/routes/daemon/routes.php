4993844823a6166b15549cce1fae4dd5499a4ef5
Lord_Ralex <lordralex@gmail.com>
2017-04-01 18:15:43 -0500

    Fix uploading of files, clean up FM UI

--- src/routes/daemon/routes.php
+++ src/routes/daemon/routes.php
@@ -128,6 +128,10 @@ $klein->respond('/daemon/server/[:serverid]/[**:path]', function($request, $resp
 
     $updatedUrl = sprintf('%s/server/%s/%s', Daemon::buildBaseUrlForNode($nodeObj->ip, $nodeObj->daemon_listen), $server, $request->param('path'));
 
+    foreach($oldHeaders as $k => $v) {
+        $header[$k] = $v;
+    }
+
     try {
         handleProxy($updatedUrl, $header, $request, $response);
     } catch (\Exception $ex) {
