60d0ac4d22be7f54f1db15433501870d8f963c71
Lord_Ralex <lordralex@gmail.com>
2017-04-12 21:45:08 -0500

    Rewrite update script to create new db and back up old This makes data restores easier and avoids leaving databases in a completely trashed state if the update fails

--- install/update.sql
+++ install/update.sql
@@ -1,11 +1,37 @@
--- Remove existing database and create new
-CREATE DATABASE IF NOT EXISTS `pufferpanel`;
-USE `pufferpanel`;
+USE pufferpanel;
 
--- Disable Foreign keys to avoid errors in dropping
 SET FOREIGN_KEY_CHECKS = 0;
 
-CREATE TABLE IF NOT EXISTS `acp_settings` (
+DROP EVENT IF EXISTS oauthTokenCleaner;
+
+ALTER TABLE account_change DROP FOREIGN KEY FK_account_change_users;
+ALTER TABLE nodes DROP FOREIGN KEY FK_nodes_locations;
+ALTER TABLE autodeploy DROP FOREIGN KEY FK_autodeploy_nodes;
+ALTER TABLE action_logs DROP FOREIGN KEY FK_actions_log_users;
+ALTER TABLE servers DROP FOREIGN KEY FK_servers_nodes;
+ALTER TABLE servers DROP FOREIGN KEY FK_servers_users;
+ALTER TABLE permissions DROP FOREIGN KEY FK_permissions_servers;
+ALTER TABLE permissions DROP FOREIGN KEY FK_permissions_users;
+ALTER TABLE subusers DROP FOREIGN KEY FK_subusers_user;
+ALTER TABLE subusers DROP FOREIGN KEY FK_subusers_server;
+ALTER TABLE oauth_access_tokens DROP FOREIGN KEY FK_oauth_access_tokens_oauth_clients;
+
+RENAME TABLE
+    acp_settings TO backup_acp_settings,
+    locations TO backup_locations,
+    users TO backup_users,
+    account_change TO backup_account_change,
+    nodes TO backup_nodes,
+    autodeploy TO backup_autodeploy,
+    actions_log TO backup_actions_log,
+    servers TO backup_servers,
+    permissions TO backup_permissions,
+    subusers TO backup_subusers,
+    oauth_clients TO backup_oauth_clients,
+    oauth_access_tokens TO backup_oauth_access_tokens
+;
+
+CREATE TABLE `acp_settings` (
   `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `setting_ref` varchar(25) NOT NULL,
   `setting_val` text,
@@ -13,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `acp_settings` (
   UNIQUE KEY `setting_ref_unique` (`setting_ref`)
 );
 
-CREATE TABLE IF NOT EXISTS `locations` (
+CREATE TABLE `locations` (
   `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `short` varchar(10) NOT NULL,
   `long` varchar(500) NOT NULL,
@@ -21,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `locations` (
   UNIQUE KEY `short_unique` (`short`)
 );
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `uuid` char(36) NOT NULL,
   `username` varchar(50) NOT NULL,
@@ -42,7 +68,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `username` (`username`)
 );
 
-CREATE TABLE IF NOT EXISTS `account_change` (
+CREATE TABLE `account_change` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL,
   `type` varchar(50) NOT NULL DEFAULT '',
@@ -55,7 +81,7 @@ CREATE TABLE IF NOT EXISTS `account_change` (
   CONSTRAINT `FK_account_change_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS `nodes` (
+CREATE TABLE `nodes` (
   `id` mediumint(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(15) NOT NULL,
   `location` mediumint(8) unsigned NOT NULL,
@@ -76,7 +102,7 @@ CREATE TABLE IF NOT EXISTS `nodes` (
   CONSTRAINT `FK_nodes_locations` FOREIGN KEY (`location`) REFERENCES `locations` (`id`)
 );
 
-CREATE TABLE IF NOT EXISTS `autodeploy` (
+CREATE TABLE `autodeploy` (
   `id` mediumint(10) unsigned NOT NULL AUTO_INCREMENT,
   `node` mediumint(10) unsigned NOT NULL,
   `code` char(36) NOT NULL,
@@ -85,7 +111,7 @@ CREATE TABLE IF NOT EXISTS `autodeploy` (
   CONSTRAINT `FK_autodeploy_nodes` FOREIGN KEY (`node`) REFERENCES `nodes` (`id`) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS `actions_log` (
+CREATE TABLE `actions_log` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `priority` tinyint(1) NOT NULL,
   `viewable` tinyint(1) unsigned NOT NULL DEFAULT '0',
@@ -100,7 +126,7 @@ CREATE TABLE IF NOT EXISTS `actions_log` (
   CONSTRAINT `FK_actions_log_users` FOREIGN KEY (`user`) REFERENCES `users` (`id`)
 );
 
-CREATE TABLE IF NOT EXISTS `servers` (
+CREATE TABLE `servers` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `hash` char(36) NOT NULL,
   `daemon_secret` char(36) NOT NULL,
@@ -116,7 +142,7 @@ CREATE TABLE IF NOT EXISTS `servers` (
   CONSTRAINT `FK_servers_users` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`)
 );
 
-CREATE TABLE IF NOT EXISTS `permissions` (
+CREATE TABLE `permissions` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user` int(10) unsigned NOT NULL,
   `server` int(10) unsigned NOT NULL,
@@ -128,7 +154,7 @@ CREATE TABLE IF NOT EXISTS `permissions` (
   CONSTRAINT `FK_permissions_users` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS `subusers` (
+CREATE TABLE `subusers` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user` int(10) unsigned NOT NULL,
   `server` int(10) unsigned NOT NULL,
@@ -139,7 +165,7 @@ CREATE TABLE IF NOT EXISTS `subusers` (
   CONSTRAINT `FK_subusers_user` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS `oauth_clients` (
+CREATE TABLE `oauth_clients` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `client_id` char(16) NOT NULL,
   `client_secret` char(64) NOT NULL,
@@ -157,7 +183,7 @@ CREATE TABLE IF NOT EXISTS `oauth_clients` (
   CONSTRAINT `FK_oauth_clients_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS `oauth_access_tokens` (
+CREATE TABLE `oauth_access_tokens` (
   `access_token` char(128) NOT NULL,
   `oauthClientId` int(10) unsigned NOT NULL,
   `expiretime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -168,12 +194,41 @@ CREATE TABLE IF NOT EXISTS `oauth_access_tokens` (
   CONSTRAINT `FK_oauth_access_tokens_oauth_clients` FOREIGN KEY (`oauthClientId`) REFERENCES `oauth_clients` (`id`) ON DELETE CASCADE
 );
 
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO acp_settings
+    SELECT * FROM acp_settings;
 
-INSERT IGNORE INTO `locations` (`id`, `short`, `long`)
-VALUES (1, 'Localhost', 'Localhost');
+INSERT INTO locations
+    SELECT * FROM backup_locations;
 
-DROP EVENT IF EXISTS `oauthTokenCleaner`;
+INSERT INTO users
+    SELECT * FROM backup_users;
+
+INSERT INTO account_change
+    SELECT * FROM backup_account_change;
+
+INSERT INTO nodes
+    SELECT * FROM backup_nodes;
+
+INSERT INTO autodeploy
+    SELECT * FROM backup_autodeploy;
+
+INSERT INTO action_logs
+    SELECT * FROM backup_actions_log;
+
+INSERT INTO servers
+    SELECT * FROM backup_servers;
+
+INSERT INTO permissions
+    SELECT * FROM backup_permissions;
+
+INSERT INTO subusers
+    SELECT * FROM backup_subusers;
+
+INSERT INTO oauth_clients
+    SELECT * FROM oauth_clients;
+
+INSERT INTO oauth_access_tokens
+    SELECT * FROM backup_oauth_access_tokens;
 
 DELIMITER //
 CREATE EVENT `oauthTokenCleaner` ON SCHEDULE EVERY 12 HOUR ON COMPLETION PRESERVE ENABLE COMMENT 'Cleans up the oauth access tokens' DO DELETE FROM oauth_access_tokens WHERE expireTime < NOW()//
@@ -182,3 +237,5 @@ DELIMITER ;
 DELIMITER //
 CREATE TRIGGER `subusers_before_delete` BEFORE DELETE ON `subusers` FOR EACH ROW DELETE FROM oauth_clients WHERE oauth_clients.user_id = OLD.user AND oauth_clients.server_id = OLD.server//
 DELIMITER ;
+
+SET FOREIGN_KEY_CHECKS = 1;
