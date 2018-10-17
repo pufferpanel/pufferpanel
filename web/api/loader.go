94e71479bd8694f46c035abe7d0e03170a43acba
Joshua Taylor <jtaylor@fsmb.org>
2018-10-17 15:47:23 -0500

    Disable locations

--- web/api/loader.go
+++ web/api/loader.go
@@ -16,10 +16,12 @@ package api
 import "github.com/gin-gonic/gin"
 
 func Register(rg *gin.RouterGroup) {
-	g := rg.Group("/locations")
-	{
-		registerLocations(g)
-	}
+	var g *gin.RouterGroup
+
+	//g := rg.Group("/locations")
+	//{
+		//registerLocations(g)
+	//}
 
 	g = rg.Group("/nodes")
 	{
