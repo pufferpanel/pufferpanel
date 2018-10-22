5a1ca7ebf61fa5e756b5c31dd840899d088be188
Joshua Taylor <jtaylor@fsmb.org>
2018-10-22 16:10:17 -0500

    Add ability to set password via view model

--- models/view/user.go
+++ models/view/user.go
@@ -5,9 +5,11 @@ import "github.com/pufferpanel/pufferpanel/models"
 type UserViewModel struct {
 	Username string `json:"username"`
 	Email    string `json:"email"`
+	//ONLY SHOW WHEN COPYING
+	Password string `json:"password,omitempty"`
 }
 
-func FromModel(model *models.User) *UserViewModel {
+func FromUser(model *models.User) *UserViewModel {
 	return &UserViewModel{
 		Username: model.Username,
 		Email:    model.Email,
@@ -21,5 +23,9 @@ func (model *UserViewModel) CopyToModel(newModel *models.User) {
 
 	if model.Email != "" {
 		newModel.Email = model.Email
+	}
+
+	if model.Password != "" {
+		newModel.SetPassword(model.Password)
 	}
 }
