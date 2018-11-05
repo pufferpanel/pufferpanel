405744002be59c8ba947a2beeea2ff6445b001da
Joshua Taylor <lordralex@gmail.com>
2018-11-05 15:57:41 -0600

    Start to try to hook scopes into mix

--- models/view/oauthtokeninfo.go
+++ models/view/oauthtokeninfo.go
@@ -1,6 +1,7 @@
 package view
 
 import (
+	oauth22 "github.com/pufferpanel/pufferpanel/oauth2"
 	"gopkg.in/oauth2.v3"
 	"time"
 )
@@ -10,13 +11,13 @@ type OAuthTokenInfoViewModel struct {
 	Mapping map[string][]string `json:"mapping,omitempty"`
 }
 
-func FromTokenInfo(info oauth2.TokenInfo) *OAuthTokenInfoViewModel {
+func FromTokenInfo(info oauth2.TokenInfo, client oauth22.ClientInfo) *OAuthTokenInfoViewModel {
 	model := &OAuthTokenInfoViewModel{}
 	model.Active = info.GetAccessCreateAt().Add(info.GetAccessExpiresIn()).After(time.Now())
 
 	//TODO: Copy data from client to mapping ONLY IF ACTIVE
 	if model.Active {
-
+		model.Mapping = client.MergeServers()
 	}
 
 	return model
--- oauth2/clientinfo.go
+++ oauth2/clientinfo.go
@@ -6,11 +6,20 @@ import (
 )
 
 type ClientInfo struct {
-	ID           uint
-	ClientID     string
-	Secret       string
-	UserID       uint
-	User         models.User
+	ID       uint
+	ClientID string
+	Secret   string
+	UserID   uint
+	User     models.User
+
+	ServerScopes []ClientServerScopes
+}
+
+type ClientServerScopes struct {
+	ID       uint
+	ServerId uint
+	Server   models.Server
+	Scope    string
 }
 
 func (ci *ClientInfo) GetSecret() string {
@@ -27,4 +36,20 @@ func (ci *ClientInfo) GetDomain() string {
 
 func (ci *ClientInfo) GetUserID() string {
 	return strconv.Itoa(int(ci.UserID))
+}
+
+func (ci *ClientInfo) MergeServers() map[string][]string {
+	mapping := make(map[string][]string, 0)
+
+	for _, v := range ci.ServerScopes {
+		temp := mapping[v.Server.Identifier]
+		if temp == nil {
+			temp = make([]string, 0)
+		}
+		temp = append(temp, v.Scope)
+
+		mapping[v.Server.Identifier] = temp
+	}
+
+	return mapping
 }
