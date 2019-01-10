8651095a01df546f9fd27eda54b1b6e6b6898e67
Lord_Ralex <lordralex@gmail.com>
2019-01-09 22:50:50 -0600

    Get login screen working

--- web/server/loader.go
+++ web/server/loader.go
@@ -2,9 +2,8 @@ package server
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/pufferpanel/pufferpanel/web/handlers"
 )
 
 func RegisterRoutes(e *gin.RouterGroup) {
-	e.GET("", handlers.AuthMiddleware, Index)
+	e.GET("", /*handlers.AuthMiddleware,*/ Index)
 }
