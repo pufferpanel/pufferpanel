a536c5cc8fd32ad121e833bb94c0342018f2309c
Lord_Ralex <lordralex@gmail.com>
2019-01-20 15:26:46 -0600

    Remove default pages from template

--- client/src/router/paths.js
+++ client/src/router/paths.js
@@ -7,46 +7,17 @@
 export default [
   {
     path: '/dashboard',
-    // Relative to /src/views
     view: 'Dashboard'
   },
   {
-    path: '/user-profile',
-    name: 'User Profile',
-    view: 'UserProfile'
-  },
-  {
-    path: '/table-list',
-    name: 'Table List',
-    view: 'TableList'
-  },
-  {
-    path: '/typography',
-    view: 'Typography'
-  },
-  {
-    path: '/icons',
-    view: 'Icons'
-  },
-  {
-    path: '/maps',
-    view: 'Maps'
-  },
-  {
-    path: '/notifications',
-    view: 'Notifications'
-  },
-  {
-    path: '/upgrade',
-    name: 'Upgrade to PRO',
-    view: 'Upgrade'
-  },
-  {
-    path: '/404',
+    path: '/error/404',
     name: 'Error',
-    view: 'Error-404',
+    view: 'errors/404',
     meta: {
-      noAuth: true
+      noAuth: true,
+      noSidebar: true,
+      noFooter: true,
+      noHeader: true
     }
   },
   {
