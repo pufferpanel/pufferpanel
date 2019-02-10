40644eca7eecff22606ed7f09a6d1c09ab89816e
Lord_Ralex <lordralex@gmail.com>
2019-02-09 23:13:27 -0600

    Add something for a server page

--- client/src/plugins/axios.js
+++ client/src/plugins/axios.js
@@ -2,6 +2,21 @@ import Vue from 'vue'
 
 // Lib imports
 import axios from 'axios'
+import Cookies from 'js-cookie'
 
 Vue.prototype.$http = axios
 Vue.prototype.axios = axios
+
+Vue.prototype.createRequest = function () {
+  let cookie = Cookies.get('puffer_auth')
+  if (cookie === undefined) {
+    cookie = ''
+  }
+  if (cookie === '') {
+    return axios.create()
+  } else {
+    return axios.create({
+      headers: { 'Authorization': 'Bearer ' + cookie }
+    })
+  }
+}
