4f569b846d5df2e73989c51a9e8c2d061150d8cc
Joshua Taylor <lordralex@gmail.com>
2019-05-15 15:27:05 -0500

    Switch to using more bootstrap vue

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,44 +1,71 @@
 <template>
-  <b-col md="6" sm="8" offset-md="3" offset-sm="2">
-    <v-card
-      title="Login">
-      <v-container>
-        <v-card-text>
-          <v-form>
-            <v-text-field
+  <b-col
+    md="6"
+    sm="8"
+    offset-md="3"
+    offset-sm="2">
+    <b-card
+      header-tag="header"
+      footer-tag="footer">
+      <h6 slot="header" class="mb-0" align="center">Login</h6>
+      <b-form>
+        <b-row class="my-1">
+          <b-col sm="1"></b-col>
+          <b-col sm="10">
+            <b-form-input
               v-model.trim="email"
               prepend-icon="mdi-account"
               name="email"
-              label="Email"
+              id="email"
+              placeholder="Email"
               type="text"/>
-            <v-text-field
+          </b-col>
+        </b-row>
+        <b-row class="my-1">
+          <b-col sm="1"></b-col>
+          <b-col sm="10">
+            <b-form-input
               v-model="password"
               prepend-icon="mdi-lock"
               name="password"
-              label="Password"
+              placeholder="Password"
               type="password"
+              id="password"
               @keyup.enter="submit"/>
-          </v-form>
-        </v-card-text>
-        <v-alert
-          v-if="error"
-          color="error"
-          v-text="error"
-        />
-      </v-container>
-      <v-container>
-        <v-card-actions>
-          <router-link to="/auth/register">Register</router-link>
-          <v-spacer/>
-          <b-btn
-            :disabled="cannotSubmit"
-            variant="primary"
-            size="sm"
-            @click="submit">Login
-          </b-btn>
-        </v-card-actions>
-      </v-container>
-    </v-card>
+          </b-col>
+        </b-row>
+      </b-form>
+      <div slot="footer">
+        <b-row>
+          <b-col sm="1"></b-col>
+          <b-col sm="2">
+            <router-link to="/auth/register">Register</router-link>
+          </b-col>
+          <b-col sm="6"></b-col>
+          <b-col sm="2">
+            <b-btn
+              :disabled="!canSubmit"
+              variant="primary"
+              size="sm"
+              @click="submit">Login
+            </b-btn>
+          </b-col>
+          <b-col sm="1"></b-col>
+        </b-row>
+        <b-row v-if="errorMsg">
+          <b-col sm="1"></b-col>
+          <b-col sm="10">
+            <b-alert
+              fade
+              dismissible
+              show="5"
+              variant="danger"
+            >{{ errorMsg }}
+            </b-alert>
+          </b-col>
+        </b-row>
+      </div>
+    </b-card>
   </b-col>
 </template>
 
@@ -51,26 +78,26 @@ export default {
       email: '',
       password: '',
       loginDisabled: false,
-      error: ''
+      errorMsg: ''
     }
   },
   computed: {
-    cannotSubmit: function () {
-      return this.loginDisabled || this.email === '' || this.password === ''
+    canSubmit: function () {
+      return !(this.loginDisabled || this.email === '' || this.password === '')
     }
   },
   methods: {
     submit () {
       let data = this
-      data.error = ''
+      data.errorMsg = ''
 
       if (!data.email) {
-        data.error = 'Email required'
+        data.errorMsg = 'Email required'
         return
       }
 
       if (!data.password) {
-        data.error = 'Password required'
+        data.errorMsg = 'Password required'
         return
       }
 
@@ -87,16 +114,16 @@ export default {
           Cookies.set('puffer_auth', responseData.data)
           this.$router.push('/server')
         } else {
-          data.error = responseData.msg
-          data.loginDisabled = false
+          data.errorMsg = responseData.msg + ''
         }
       }).catch(function (error) {
-        let msg = error.message
+        let msg = error.message + ''
         if (error && error.response && error.response.data && error.response.data.msg) {
-          msg = error.response.data.msg
+          msg = error.response.data.msg + ''
         }
 
-        data.error = msg
+        data.errorMsg = msg
+      }).finally(function () {
         data.loginDisabled = false
       })
     }
