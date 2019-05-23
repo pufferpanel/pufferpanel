8051fe8e99e6494be640ba5c56c17f6b80118737
Joshua Taylor <lordralex@gmail.com>
2019-05-23 16:20:20 -0500

    Random code for getting servers set up

--- client/src/components/core/servers/generic.vue
+++ client/src/components/core/servers/generic.vue
@@ -1,11 +1,20 @@
 <template>
+  <b-container>
 
+  </b-container>
 </template>
 
 <script>
+import servers from '@/plugins/servers.js'
+
 export default {
   props: {
     server: Object
+  },
+  methods: {
+    loadConsole() {
+      servers.connect(this.server);
+    }
   }
 }
 </script>
