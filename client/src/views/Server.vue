8d09976366f774546db04c526dd2df742f9d3b35
Joshua Taylor <lordralex@gmail.com>
2019-05-23 16:20:20 -0500

    Random code for getting servers set up

--- client/src/views/Server.vue
+++ client/src/views/Server.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <b-container v-if="this.server">
-      <keep-alive>
-        <core-servers-minecraft v-if="server.type === 'minecraft'"></core-servers-minecraft>
-        <core-servers-generic v-else v-bind:server="server"></core-servers-generic>
-      </keep-alive>
-    </b-container>
+    <div v-if="this.server">
+      <!--<keep-alive>-->
+        <!--<core-servers-minecraft v-if="server.type === 'minecraft'"></core-servers-minecraft>
+        <core-servers-generic v-else v-bind:server="server"></core-servers-generic>-->
+      <core-servers-generic v-bind:server="server"></core-servers-generic>
+      <!--</keep-alive>-->
+    </div>
     <b-row v-else>
       <b-col cols="5"/>
       <b-col cols="2">
