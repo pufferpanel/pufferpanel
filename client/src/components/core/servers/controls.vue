fe70f9cc8f74faea85ffee703ee4bc1a0fa19e5e
Joshua Taylor <lordralex@gmail.com>
2019-05-31 16:32:08 -0500

    We can now start and stop servers!

--- client/src/components/core/servers/controls.vue
+++ client/src/components/core/servers/controls.vue
@@ -12,26 +12,25 @@
   -->
 
 <template>
-  <b-container>
-    <h2 v-text="server.name"></h2>
-    <br>
-    <b-row>
-      <b-col>
-        <core-servers-console v-bind:server="server"></core-servers-console>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        <core-servers-controls></core-servers-controls>
-      </b-col>
-    </b-row>
-  </b-container>
+  <b-button-group>
+    <b-button variant="success" v-on:click="start">Start</b-button>
+    <b-button variant="warning" v-on:click="stop">Stop</b-button>
+    <b-button variant="danger" v-on:click="kill">Kill</b-button>
+  </b-button-group>
 </template>
 
 <script>
 export default {
-  props: {
-    server: Object
+  methods: {
+    start () {
+      this.$socket.sendObj({ type: 'start' })
+    },
+    stop () {
+      this.$socket.sendObj({ type: 'stop' })
+    },
+    kill () {
+      this.$socket.sendObj({ type: 'kill' })
+    }
   }
 }
 </script>
