905b10505b87fd2c7f3a4e18ec9de9eb3c9051c3
Joshua Taylor <lordralex@gmail.com>
2019-07-31 15:16:40 -0500

    Add calls to get nodes and their template and render

--- client/src/views/AddServer.vue
+++ client/src/views/AddServer.vue
@@ -15,19 +15,20 @@
   <b-container>
     <b-row>
       <b-col cols="2">
-        <label :for="`nodeSelect`" v-text="$t('common.Node')"></label>
+        <label :for="`nodeSelect`"><strong v-text="$t('common.Node')"></strong></label>
       </b-col>
       <b-col cols="10">
-        <b-form-select id="nodeSelect" v-model="selectedNode" :options="nodes"></b-form-select>
+        <b-form-select :disabled="loadingNodes" id="nodeSelect" v-model="selectedNode" :options="nodes"></b-form-select>
       </b-col>
     </b-row>
 
     <b-row>
       <b-col cols="2">
-        <label :for="`templateSelect`" v-text="$t('common.Template')"></label>
+        <label :for="`templateSelect`"><strong v-text="$t('common.Template')"></strong></label>
       </b-col>
       <b-col cols="10">
-        <b-form-select id="templateSelect" v-model="selectedTemplate" :options="templates"></b-form-select>
+        <b-form-select :disabled="loadingTemplates" id="templateSelect" v-model="selectedTemplate"
+                       :options="templates"></b-form-select>
       </b-col>
     </b-row>
   </b-container>
@@ -40,9 +41,13 @@ export default {
       nodes: [],
       selectedNode: null,
       templates: [],
+      templateData: {},
       selectedTemplate: null,
       formData: {},
-      readme: ''
+      readme: '',
+
+      loadingNodes: true,
+      loadingTemplates: true
     }
   },
   watch: {
@@ -53,24 +58,69 @@ export default {
   methods: {
     getTemplates () {
       console.log('Getting templates for node ' + this.selectedNode)
+      let vue = this
+      this.loadingTemplates = true
+      this.templates = [{
+        value: null,
+        disabled: true,
+        text: this.$t('common.Loading')
+      }]
+      this.templateData = {}
+      this.selectedTemplate = null
+      this.$http.get('/daemon/node/' + this.selectedNode + '/templates').then(function (res) {
+        let response = res.data
+        if (response.success) {
+          vue.templates = [{
+            value: null,
+            disabled: true,
+            text: vue.$t('common.SelectTemplate')
+          }]
+          vue.templateData = response.data
+          for (let k in vue.templateData) {
+            vue.templates.push({
+              text: vue.templateData[k].display,
+              value: k
+            })
+          }
+
+          if (vue.templates.length === 2) {
+            vue.selectedTemplate = vue.templates[1].value
+          } else {
+          }
+
+          vue.loadingTemplates = false
+        }
+      })
     }
   },
   mounted () {
     let vue = this
+    this.nodes = [{
+      value: null,
+      disabled: true,
+      text: this.$t('common.Loading')
+    }]
     this.$http.get('/api/nodes').then(function (res) {
       let callResult = res.data
       if (callResult.success) {
         for (let i = 0; i < callResult.data.length; i++) {
           let node = callResult.data[i]
+          vue.nodes = [{
+            value: null,
+            disabled: true,
+            text: vue.$t('common.SelectNode')
+          }]
           vue.nodes.push({
             value: node.id,
             text: node.name
           })
         }
 
-        if (vue.nodes.length === 1) {
-          vue.selectedNode = vue.nodes[0].value
+        if (vue.nodes.length === 2) {
+          vue.selectedNode = vue.nodes[1].value
         }
+
+        vue.loadingNodes = false
       }
     })
   }
