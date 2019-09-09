015683d122d4bc3c91ab8125f8f67b23d694a861
Joshua Taylor <lordralex@gmail.com>
2019-09-09 16:10:34 -0500

    Add partial user management for servers Does not allow you to add users, but you can edit a user's roles and remove them

--- client/src/components/core/servers/users.vue
+++ client/src/components/core/servers/users.vue
@@ -17,10 +17,69 @@
     <h6 slot="header" class="mb-0">
       <span v-text="$t('common.Users')"></span>
     </h6>
+    <b-card v-for="user in users" header-tag="header">
+      <h6 slot="header" class="mb-0">
+        <span v-text="user.username"></span>
+      </h6>
+      <b-card-text>
+        <b-form-group>
+          <b-form-checkbox-group
+            v-model="user.scopes"
+            :options="options"
+            switches
+            stacked
+          ></b-form-checkbox-group>
+          <b-button size="sm" v-on:click="updateUser(user)" v-text="$t('common.Update')" variant="primary"></b-button>
+        </b-form-group>
+      </b-card-text>
+    </b-card>
   </b-card>
 
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      users: [],
+      options: [
+        { text: this.$t('scopes.ServersView'), value: 'servers.view' },
+        { text: this.$t('scopes.ServersEdit'), value: 'servers.edit' },
+        { text: this.$t('scopes.ServersInstall'), value: 'servers.install' },
+        { text: this.$t('scopes.ServersConsole'), value: 'servers.console' },
+        { text: this.$t('scopes.ServersConsoleSend'), value: 'servers.console.send' },
+        { text: this.$t('scopes.ServersStop'), value: 'servers.stop' },
+        { text: this.$t('scopes.ServersStart'), value: 'servers.start' },
+        { text: this.$t('scopes.ServersStat'), value: 'servers.stat' },
+        { text: this.$t('scopes.ServersFiles'), value: 'servers.files' },
+        { text: this.$t('scopes.ServersFilesGet'), value: 'servers.files.get' },
+        { text: this.$t('scopes.ServersFilesPut'), value: 'servers.files.put' },
+        { text: this.$t('scopes.ServersEditUsers'), value: 'servers.edit.users' },
+      ]
+    }
+  },
+  mounted () {
+    this.loadUsers()
+  },
+  methods: {
+    loadUsers () {
+      let vue = this
+      this.$http.get('/api/servers/' + this.$route.params.id + '/user').then(function (response) {
+        vue.users = response.data.data
+      })
+    },
+    updateUser (user) {
+      let vue = this
+      if (user.scopes.length === 0) {
+        this.$http.delete('/api/servers/' + this.$route.params.id + '/user/' + user.username).then(function (response) {
+          vue.loadUsers()
+        })
+      } else {
+        this.$http.put('/api/servers/' + this.$route.params.id + '/user/' + user.username, user).then(function (response) {
+          vue.loadUsers()
+        })
+      }
+    }
+  }
+}
 </script>
