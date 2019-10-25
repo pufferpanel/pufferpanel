9b07138858831e93dfd9720544623bd141c1d309
Joshua Taylor <lordralex@gmail.com>
2019-10-25 15:28:41 -0500

    Add user add

--- cmd/template.go
+++ cmd/template.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-	"github.com/pufferpanel/pufferpanel/v2/cmd/templates"
+	"github.com/pufferpanel/pufferpanel/v2/cmd/template"
 	"github.com/spf13/cobra"
 )
 
 var templatesCmd = &cobra.Command{
 	Use:   "template",
 	Short: "Manage templates",
-	Run:   executeRun,
 }
 
 func init() {
 	templatesCmd.AddCommand(
-		templates.AddCmd,
-		templates.ImportCmd)
+		template.AddCmd,
+		template.ImportCmd)
 }
