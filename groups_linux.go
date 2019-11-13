46e13d12b5224beb4163cf882634fd698d96358b
Joshua Taylor <lordralex@gmail.com>
2019-11-13 16:31:21 -0600

    Change group check to permit pufferpanel and root groups This means root can use this without changing groups

--- groups_linux.go
+++ groups_linux.go
@@ -10,14 +10,28 @@ func UserInGroup() bool {
 		return false
 	}
 
-	expectedGroup, err := user.LookupGroup("pufferpanel")
-	if err != nil {
-		return false
-	}
 	groups, err := u.GroupIds()
 	if err != nil {
 		return false
 	}
 
-	return ContainsString(groups, expectedGroup.Gid)
+	allowedIds := make([]string, 0)
+
+	if expectedGroup, err := user.LookupGroup("pufferpanel"); err == nil {
+		allowedIds = append(allowedIds, expectedGroup.Gid)
+	}
+
+	if rootGroup, err := user.LookupGroup("root"); err == nil {
+		allowedIds = append(allowedIds, rootGroup.Gid)
+	}
+
+	for _, v := range groups {
+		for _, t := range allowedIds {
+			if v == t {
+				return true
+			}
+		}
+	}
+
+	return false
 }
