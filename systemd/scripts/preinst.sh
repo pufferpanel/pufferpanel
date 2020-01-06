5cd06e0d44c75c59b53a8ccc0d5a97a9d701834d
Joshua Taylor <lordralex@gmail.com>
2020-01-06 13:17:30 -0600

    Clean up how systemd files are located and used

--- systemd/scripts/preinst.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-useradd --system --home /var/lib/pufferpanel --user-group pufferpanel
-mkdir -p /etc/pufferpanel /var/log/pufferpanel /var/lib/pufferpanel
-chown pufferpanel:pufferpanel /etc/pufferpanel /var/log/pufferpanel /var/lib/pufferpanel
