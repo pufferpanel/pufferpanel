49e914d75cdb97b26d485f44c529d45267bd6a12
Joshua Taylor <lordralex@gmail.com>
2020-03-02 10:59:40 -0600

    Kill WS on error, stop sending data on first connection

--- daemon/web/server/server.go
+++ daemon/web/server/server.go
@@ -683,11 +683,6 @@ func OpenSocket(c *gin.Context) {
 		return
 	}
 
-	if _, ok := c.GetQuery("skipReplay"); !ok {
-		console, _ := program.GetEnvironment().GetConsole()
-		_ = pufferpanel.Write(conn, messages.Console{Logs: console})
-	}
-
 	internalMap, _ := c.Get("scopes")
 	scopes := internalMap.([]pufferpanel.Scope)
 
