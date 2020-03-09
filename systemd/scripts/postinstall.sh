45c244b31613a9aa3be0a5f3bd958edcd93fa086
Joshua Taylor <lordralex@gmail.com>
2020-03-09 16:16:05 -0500

    Add upgrade scripts [skip]

--- systemd/scripts/postinstall.sh
+++ systemd/scripts/postinstall.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
 systemctl daemon-reload
+
 if [ ! -f "/var/lib/pufferpanel/database.db" ]; then
   touch /var/lib/pufferpanel/database.db
 fi
+
 chown -R pufferpanel:pufferpanel /etc/pufferpanel /var/log/pufferpanel /var/lib/pufferpanel
