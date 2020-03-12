81503ca9e5ab46a31b3298a9f2f6f5f8c405c802
Joshua Taylor <lordralex@gmail.com>
2020-03-12 16:43:15 -0500

    Alter how signals are listened for [save]

--- cmd/run.go
+++ cmd/run.go
@@ -1,5 +1,5 @@
 /*
- Copyright 2019 Padduck, LLC
+ Copyright 2020 Padduck, LLC
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
@@ -58,6 +58,8 @@ func internalRun(cmd *cobra.Command, args []string) error {
 	defer logging.Close()
 
 	c := make(chan error)
+
+	signal.Ignore(syscall.SIGPIPE, syscall.SIGHUP)
 
 	go func() {
 		quit := make(chan os.Signal)
