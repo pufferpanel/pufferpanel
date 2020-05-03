d2bf8ec04ff00cacfe6db9adf22065a5f05fa78d
Lord_Ralex <lordralex@gmail.com>
2020-05-03 18:06:45 -0500

    Remove login scope

--- web/auth/register.go
+++ web/auth/register.go
@@ -58,7 +58,6 @@ func RegisterPost(c *gin.Context) {
 	}
 
 	perms.ViewServer = true
-	perms.Login = true
 
 	err = ps.UpdatePermissions(perms)
 	if response.HandleError(c, err, http.StatusInternalServerError) {
