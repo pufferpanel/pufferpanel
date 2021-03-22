3b89249b2e478a478c2c20d77cbd51e408bf500c
Joshua Taylor <lordralex@gmail.com>
2021-03-22 14:01:37 -0500

    Change scripts to handle errors a bit more correctly

--- systemd/scripts/preinstall.sh
+++ systemd/scripts/preinstall.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 
-useradd --system --home /var/lib/pufferpanel --user-group pufferpanel || true
+useradd --system --home /var/lib/pufferpanel --user-group pufferpanel
+
+exitCode=$?
+[ $exitCode -ne 0 && $exitCode -ne 9 ] || exit $exitCode
--- systemd/scripts/preremove.sh
+++ systemd/scripts/preremove.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-systemctl stop pufferpanel || true
+systemctl stop pufferpanel
