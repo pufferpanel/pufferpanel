e5b1441cf0caa2bfc26f73af06d22b423c1e5aa2
nepcore <nepcore@griem.xyz>
2021-05-08 00:39:46 +0200

    change user settings value column to be of type text

--- models/usersetting.go
+++ models/usersetting.go
@@ -3,7 +3,7 @@ package models
 type UserSetting struct {
 	Key    string `gorm:"NOT NULL;size:100;PRIMARY_KEY"`
 	UserID uint   `gorm:"NOT NULL;PRIMARY_KEY;AUTO_INCREMENT:false"`
-	Value  string `gorm:"NOT NULL;size:200"`
+	Value  string `gorm:"NOT NULL;type:text"`
 }
 
 type UserSettings []*UserSetting
