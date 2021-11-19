bf4f4551e1241e32e992b1c3acaab479b79d03f7
Joshua Taylor <lordralex@gmail.com>
2021-11-19 11:54:04 -0600

    Add firewall rules

--- build.bat
+++ build.bat
@@ -1,7 +1,13 @@
+set VERSION=1.0.0
+
+xcopy /E /I /Y client\dist www
+xcopy /E /I /Y assets\email email
+
 heat dir www -out www.wxs -gg -g1 -ke -nologo -cg WwwComponent -dr APPLICATIONROOTDIRECTORY
 heat dir email -out email.wxs -gg -g1 -ke -nologo -cg EmailComponent -dr APPLICATIONROOTDIRECTORY
-candle *.wxs -arch x64 -dProductVersion="%VERSION%"
+candle *.wxs -arch x64 -dProductVersion="%VERSION%" -ext WixFirewallExtension
 del www.wxs email.wxs
-light *.wixobj -out pufferpanel.msi -b email -b www -ext WixUIExtension -cultures:en-us
+light *.wixobj -out pufferpanel.msi -b email -b www -ext WixUtilExtension -ext WixUIExtension -ext WixFirewallExtension -cultures:en-us
 del *.wixobj *wixpdb
-pause
+del /f www
+del /f email
