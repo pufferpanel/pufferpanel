704e00b104200c72c70ccaaf7d96fb7347898f42
Joshua Taylor <lordralex@gmail.com>
2023-02-20 13:17:41 -0600

    Remove new migration This won't work in the v2.6.2 era onward, so safer to not ship it now

--- database/migrate.go
+++ database/migrate.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"github.com/go-gormigrate/gormigrate/v2"
-	"github.com/pufferpanel/pufferpanel/v2/config"
 	"github.com/pufferpanel/pufferpanel/v2/models"
 	"gorm.io/gorm"
 )
@@ -30,42 +29,6 @@ func migrate(dbConn *gorm.DB) error {
 				var local *models.Node
 				for _, n := range nodes {
 					if (n.PrivateHost == "localhost" || n.PrivateHost == "127.0.0.1") && (n.PublicHost == "localhost" || n.PublicHost == "127.0.0.1") {
-						local = n
-					}
-				}
-
-				if local == nil {
-					return nil
-				}
-
-				err = db.Table("servers").Where("node_id = ?", local.ID).Update("node_id", nil).Error
-				if err != nil {
-					return err
-				}
-				err = db.Delete(local).Error
-				if err != nil {
-					return err
-				}
-
-				return nil
-			},
-		},
-		{
-			ID: "1676911364",
-			Migrate: func(db *gorm.DB) error {
-				if !config.DaemonEnabled.Value() {
-					return nil
-				}
-
-				var nodes []*models.Node
-				err := db.Find(&nodes).Error
-				if err != nil {
-					return err
-				}
-
-				var local *models.Node
-				for _, n := range nodes {
-					if n.ID == 1 {
 						local = n
 					}
 				}
