396c6da7f88ef04a828da4de51ba8f2211c62bd1
James Kerr <disconsented@users.noreply.github.com>
2023-06-03 20:06:54 +1200

    Update console.go

--- operations/message/console.go
+++ operations/message/console.go
@@ -1,5 +1,5 @@
 /*
- Copyright 2016 Padduck, LLC
+ Copyright 2023 Padduck, LLC
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
