3d2d160ff831d9b37555c32ea76df41849432bf4
Joshua Taylor <lordralex@gmail.com>
2023-06-29 12:18:20 -0500

    Fix paths for linux steam

--- operations/steamgamedl/metadata_notwindows.go
+++ operations/steamgamedl/metadata_notwindows.go
@@ -6,8 +6,8 @@ const SteamMetadataLink = SteamMetadataServerLink + "steam_cmd_linux"
 const DownloadOs = "linux"
 
 var RenameFolders = map[string]string{
-	"steam32": "sdk32",
-	"steam64": "sdk64",
+	"linux32": "sdk32",
+	"linux64": "sdk64",
 }
 
 const DepotDownloaderLink = "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/DepotDownloader-linux-${arch}.zip"
