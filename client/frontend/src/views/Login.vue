6cde0a76bf28b406fed2698f76fe59f3609354b5
nepcore <nepcore@griem.xyz>
2023-10-13 11:56:49 +0200

    slight improvements for the login page

--- client/frontend/src/views/Login.vue
+++ client/frontend/src/views/Login.vue
@@ -10,11 +10,13 @@ import defaultRoute from '@/router/defaultRoute'
 const { t } = useI18n()
 const api = inject('api')
 const events = inject('events')
-const toast = inject('toast')
+const validate = inject('validate')
 const router = useRouter()
 
 const email = ref('')
+const emailError = ref(false)
 const password = ref('')
+const passwordError = ref(false)
 const otpNeeded = ref(false)
 const token = ref('')
 
@@ -46,15 +48,58 @@ async function submitOtp() {
   await api.auth.loginOtp(token.value)
   loggedIn()
 }
+
+const timeouts = {}
+function now(f) {
+  if (timeouts[f.name]) {
+    clearTimeout(timeouts[f.name])
+    delete timeouts[f.name]
+  }
+  f()
+}
+
+function wait(f) {
+  if (timeouts[f.name]) {
+    clearTimeout(timeouts[f.name])
+  }
+  timeouts[f.name] = setTimeout(() => {
+    delete timeouts[f.name]
+    f()
+  }, 1000)
+}
+
+function validateEmail() {
+  if (!validate.email(email.value)) {
+    emailError.value = true
+  } else {
+    emailError.value = false
+  }
+}
+
+function emailErrorMsg() {
+  if (emailError.value) return t('errors.ErrEmailInvalid')
+}
+
+function validatePassword() {
+  if (!validate.password(password.value)) {
+    passwordError.value = true
+  } else {
+    passwordError.value = false
+  }
+}
+
+function passwordErrorMsg() {
+  if (passwordError.value) return t('errors.ErrPasswordRequirements')
+}
 </script>
 
 <template>
   <div class="login">
     <h1 v-text="t('users.Login')" />
     <form @keydown.enter="login()">
-      <text-field v-model="email" type="email" name="email" :label="t('users.Email')" icon="email" autofocus />
-      <text-field v-model="password" type="password" name="password" :label="t('users.Password')" icon="lock" />
-      <btn color="primary" @click="login()" v-text="t('users.Login')" />
+      <text-field v-model="email" type="email" name="email" :label="t('users.Email')" :error="emailErrorMsg()" icon="email" autofocus @blur="now(validateEmail)" @change="wait(validateEmail)" />
+      <text-field v-model="password" type="password" name="password" :label="t('users.Password')" :error="passwordErrorMsg()" icon="lock" @blur="now(validatePassword)" @change="wait(validatePassword)" />
+      <btn color="primary" :disabled="emailError || passwordError" @click="login()" v-text="t('users.Login')" />
       <btn v-if="$config.registrationEnabled" variant="text" @click="$router.push({ name: 'Register' })" v-text="t('users.RegisterLink')" />
     </form>
     <overlay v-model="otpNeeded" :title="t('users.OtpNeeded')" closable @close="resetOtp()">
