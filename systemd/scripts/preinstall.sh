93f86460879cb2f86e053799fdd9ad73be76d77c
Joshua Taylor <lordralex@gmail.com>
2024-03-25 11:29:05 -0500

    Revert the script Damn you docker

--- systemd/scripts/preinstall.sh
+++ systemd/scripts/preinstall.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env sh
 
-if [ -n "${CODESPACES}" ]; then
-  echo 'CODESPACE IS NOT A PERMITTED PLATFORM!'
-  echo 'DO NOT USE CODESPACES TO RUN THIS PACKAGE'
-  echo 'THIS IS A VIOLATION OF GITHUB TOS'
-  echo 'DO NOT ASK HOW TO GET AROUND IT.'
-  exit 1
-fi
-
 useradd --system --home /var/lib/pufferpanel --user-group pufferpanel
 
 exitCode=$?
