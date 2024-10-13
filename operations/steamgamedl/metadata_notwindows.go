76f26b9a7aeb627de0f68b914cb414a5a4246696
Joshua Taylor <lordralex@gmail.com>
2024-10-12 22:31:46 -0500

    Update depot downloader

--- operations/steamgamedl/metadata_notwindows.go
+++ operations/steamgamedl/metadata_notwindows.go
@@ -4,5 +4,5 @@ package steamgamedl
 
 const SteamMetadataLink = SteamMetadataServerLink + "steam_cmd_linux"
 const DownloadOs = "linux"
-const DepotDownloaderLink = "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/DepotDownloader-linux-${arch}.zip"
+const DepotDownloaderLink = "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.7.2/DepotDownloader-linux-${arch}.zip"
 const DepotDownloaderBinary = "DepotDownloader"
--- operations/steamgamedl/metadata_windows.go
+++ operations/steamgamedl/metadata_windows.go
@@ -3,5 +3,5 @@ package steamgamedl
 const SteamMetadataLink = SteamMetadataServerLink + "steam_cmd_win32"
 const DownloadOs = "win32"
 
-const DepotDownloaderLink = "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/DepotDownloader-windows-${arch}.zip"
+const DepotDownloaderLink = "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.7.2/DepotDownloader-windows-${arch}.zip"
 const DepotDownloaderBinary = "DepotDownloader.exe"
