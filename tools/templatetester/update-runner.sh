6131bdefddf8ef093a32601a300f26e67a39e617
Joshua Taylor <lordralex@gmail.com>
2025-03-03 09:33:35 -0600

    Just so I can avoid doing this in the future.... Ansible time! [skip]
    Signed-off-by: Joshua Taylor <lordralex@gmail.com>

--- tools/templatetester/update-runner.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-helm upgrade --namespace arc-runners -f docker.yaml  -f template-tester.yaml template-tester oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
