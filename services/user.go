4464bc5f6222a7fe5f7ded6ddbc321ba37eee9a8
nepcore <nepcore@griem.xyz>
2025-09-17 19:11:43 +0200

    Correct reported password max field length

--- services/user.go
+++ services/user.go
@@ -343,7 +343,7 @@ func (us *User) Search(usernameFilter, emailFilter string, pageSize, page uint) 
 func (us *User) IsSecurePassword(password string) error {
 	//TODO: Change to use validator
 	if len(password) < 8 {
-		return pufferpanel.ErrFieldLength("password", 8, 100)
+		return pufferpanel.ErrFieldLength("password", 8, 72)
 	}
 	return nil
 }
